b0c7a1ff000787c36f893becce91fdf165688688
Dominik Berner <dominik.berner@contractors.roche.com>
2018-11-05 14:52:21 +0100

    Initial commit; building set up

---
+++ test/Catch2Main.cc
@@ -0,0 +1,7 @@
+/*
+This file is split of from the test cases to help with incremental builds
+*/
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this \
+                          // in one cpp file
+
+#include <catch.hpp>
