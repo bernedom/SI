143a59953aa1ce0a4a68a3ae4294dcb4f17ef55c
Dominik Berner <dominik.berner@gmail.com>
2018-11-06 12:49:40 +0100

    Straigth forward templatizing using ratio

--- include/length.h
+++ include/length.h
@@ -1,14 +1,20 @@
 #include <limits>
+#include <ratio>
 
+template <class R = std::ratio<1>>
 struct length_t
 {
+    typedef R ratio;
     typedef long long int internal_type;
     constexpr length_t(long long int l) : length_{l} {}
     internal_type length_;
 
-    constexpr bool operator==(const length_t &rhs) const
+    template <class Rr = std::ratio<1>>
+    constexpr bool operator==(const length_t<Rr> &rhs) const
     {
-        return rhs.length_ == length_;
+        return (rhs.length_ * length_t<Rr>::ratio::num) /
+                   length_t<Rr>::ratio::den ==
+               (length_ * ratio::num) / ratio::den;
     }
     constexpr length_t operator*(const long long int f) const
     {
@@ -18,27 +24,28 @@ struct length_t
     constexpr length_t operator-() { return {-length_}; }
 };
 
-constexpr length_t operator"" _m(const unsigned long long int m)
+constexpr length_t<std::ratio<1>>
+operator"" _m(const unsigned long long int m)
 {
-    if (m > std::numeric_limits<length_t::internal_type>::max())
+    if (m > std::numeric_limits<length_t<std::ratio<1>>::internal_type>::max())
     {
         throw std::overflow_error("supplied value too large");
     }
-    return m * 100;
+    return m;
 }
 
-constexpr length_t operator"" _km(const unsigned long long int km)
+constexpr length_t<std::kilo> operator"" _km(const unsigned long long int km)
 {
-    if (km > std::numeric_limits<length_t::internal_type>::max())
+    if (km > std::numeric_limits<length_t<std::kilo>::internal_type>::max())
     {
         throw std::overflow_error("supplied value too large");
     }
-    return km * 100000;
+    return km;
 }
 
-constexpr length_t operator"" _cm(const unsigned long long int cm)
+constexpr length_t<std::centi> operator"" _cm(const unsigned long long int cm)
 {
-    if (cm > std::numeric_limits<length_t::internal_type>::max())
+    if (cm > std::numeric_limits<length_t<std::centi>::internal_type>::max())
     {
         throw std::overflow_error("supplied value too large");
     }
--- test/src/length_tests.cc
+++ test/src/length_tests.cc
@@ -5,9 +5,16 @@
 #include <length.h>
 
 // stream operator needed to expand in test output
-std::ostream &operator<<(std::ostream &os, const length_t &l) {
+template <class R = std::ratio<1>>
+std::ostream &operator<<(std::ostream &os, const length_t<R> &l) {
   os << l.length_;
   return os;
+}
+
+TEST_CASE("km returns ratio 1000 to 1") {
+  const auto one_k = 1_km;
+  REQUIRE(decltype(one_k)::ratio::num == 1000);
+  REQUIRE(decltype(one_k)::ratio::den == 1);
 }
 
 TEST_CASE("one km is tousand m") {
