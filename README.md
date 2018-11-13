d3b9da575946e0c5f21307879c85bc39c68d475c
Dominik Berner <dominik.berner@gmail.com>
2018-11-13 16:18:51 +0100

    Added m and mm for floating point lengths

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # SI
-A header only c++ library that uses user defined literals to help with SI-unit conversion/ 
+A header only c++ library that uses user defined literals to help with SI-unit conversion at compile time. 
 
-This is a learning project to figure out modern C++ user defined literals and probably some template and type-traits magic as well
+
+This is a learning project to figure out modern C++ user defined literals and probably some template and type-traits magic as well. 
--- test/src/length_tests_floating_point.cc
+++ test/src/length_tests_floating_point.cc
@@ -4,12 +4,28 @@
 
 // stream operator needed to expand in test output
 template <class R = std::ratio<1>>
-std::ostream &operator<<(std::ostream &os, const length_t<R> &l) {
+std::ostream &operator<<(std::ostream &os, const length_t<R> &l)
+{
   os << l.raw_value();
   return os;
 }
-TEST_CASE("km returns ratio 1000 to 1 floating point") {
+TEST_CASE("km returns ratio 1000 to 1 floating point")
+{
   const auto one_k = 1.0_km;
   static_assert(decltype(one_k)::ratio::num == 1000, "one km is 1000 / 1");
   static_assert(decltype(one_k)::ratio::den == 1, "one km is 1000/ 1");
+}
+
+TEST_CASE("mm returns ratio 1 to 1000 floating point")
+{
+  const auto one_mm = 1.0_mm;
+  static_assert(decltype(one_mm)::ratio::num == 1, "one mm is 1/ 1000");
+  static_assert(decltype(one_mm)::ratio::den == 1000, "one kmmm is 1/ 1000");
+}
+
+TEST_CASE("m returns ratio 1 to 1 floating point")
+{
+  const auto one_m = 1.0_m;
+  static_assert(decltype(one_m)::ratio::num == 1, "one mm is 1/ 1");
+  static_assert(decltype(one_m)::ratio::den == 1, "one kmmm is 1/ 1");
 }
