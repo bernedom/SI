39ff726ae537524337f0ccc5ba6fa4fd80eec351
Dominik Berner <dominik.berner@gmail.com>
2018-11-20 12:38:15 +0100

    Added overflow check to time unit generation

--- include/SI/time.h
+++ include/SI/time.h
@@ -4,16 +4,19 @@
 #include <ratio>
 
 #include "value_holder.h"
-namespace SI {
+namespace SI
+{
 
 template <class R = std::ratio<1>, typename T = long long int>
-struct time_t : public value_holder_t<R, T> {
+struct time_t : public value_holder_t<R, T>
+{
   using value_holder_t<R, T>::value_holder_t;
 
   using value_holder_t<R, T>::operator*;
 };
 } // namespace SI
 
-constexpr auto operator"" _s(unsigned long long int s) {
-  return SI::time_t<std::ratio<1>, long long int>(s);
+constexpr auto operator"" _s(unsigned long long int s)
+{
+  return SI::detail::generate_unit_type_overflow_check<SI::time_t, std::ratio<1>>(s);
 }
--- test/src/time_tests.cc
+++ test/src/time_tests.cc
@@ -2,9 +2,10 @@
 
 #include <SI/time.h>
 
-TEST_CASE("s returns ratio 1 to 1")
-{
-    constexpr auto one = 1_s;
-    static_assert(decltype(one)::ratio::num == 1, "One km is 1000 / 1");
-    static_assert(decltype(one)::ratio::den == 1, "One km is 1000 / 1");
+TEST_CASE("s returns ratio 1 to 1") {
+  constexpr auto one = 1_s;
+  static_assert(decltype(one)::ratio::num == 1, "One km is 1000 / 1");
+  static_assert(decltype(one)::ratio::den == 1, "One km is 1000 / 1");
 }
+
+TEST_CASE("Assert on overflow for s") { REQUIRE_THROWS(0xffffffffffffffff_s); }
