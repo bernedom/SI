2b73743a71914894e51a937d2c88886885e98970
Dominik Berner <dominik.berner@gmail.com>
2018-11-21 12:58:42 +0100

    Wrapped std::chrono for time values

--- include/SI/time.h
+++ include/SI/time.h
@@ -4,22 +4,26 @@
 #include <ratio>
 
 #include "value_holder.h"
-namespace SI {
+namespace SI
+{
 
-template <class R = std::ratio<1>, typename T = long long int>
-struct time_t : public value_holder_t<R, T> {
+template <class R = std::ratio<1>, typename T = std::chrono::seconds>
+struct time_t : public value_holder_t<R, T>
+{
   using value_holder_t<R, T>::value_holder_t;
 
   using value_holder_t<R, T>::operator*;
 };
 } // namespace SI
 
-constexpr auto operator"" _s(unsigned long long int s) {
+constexpr auto operator"" _s(unsigned long long int s)
+{
   return SI::time_t<std::chrono::seconds::period, std::chrono::seconds>(
       std::chrono::seconds(s));
 }
 
-constexpr auto operator"" _min(unsigned long long int min) {
+constexpr auto operator"" _min(unsigned long long int min)
+{
   return SI::time_t<std::chrono::minutes::period, std::chrono::minutes>(
       std::chrono::minutes(min));
 }
--- test/src/time_tests.cc
+++ test/src/time_tests.cc
@@ -14,4 +14,9 @@ TEST_CASE("min returns ratio 60 to 1") {
   static_assert(decltype(one)::ratio::den == 1, "One min is 60 / 1");
 }
 
+TEST_CASE("negative duration test") {
+  constexpr auto minus_one = -1_s;
+  REQUIRE(minus_one.raw_value().count() == -1);
+}
+
 TEST_CASE("Assert on overflow for s") { REQUIRE_NOTHROW(0xffffffffffffffff_s); }
