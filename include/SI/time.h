cc7afe4fd80bcdab3d67686cbee83ea11bb650dc
Dominik Berner <dominik.berner@gmail.com>
2018-11-21 12:44:09 +0100

    using std::chrono for time durations

--- include/SI/time.h
+++ include/SI/time.h
@@ -1,22 +1,25 @@
 #pragma once
 
-//#include <chrono>
+#include <chrono>
 #include <ratio>
 
 #include "value_holder.h"
-namespace SI
-{
+namespace SI {
 
 template <class R = std::ratio<1>, typename T = long long int>
-struct time_t : public value_holder_t<R, T>
-{
+struct time_t : public value_holder_t<R, T> {
   using value_holder_t<R, T>::value_holder_t;
 
   using value_holder_t<R, T>::operator*;
 };
 } // namespace SI
 
-constexpr auto operator"" _s(unsigned long long int s)
-{
-  return SI::detail::generate_unit_type_overflow_check<SI::time_t, std::ratio<1>>(s);
+constexpr auto operator"" _s(unsigned long long int s) {
+  return SI::time_t<std::chrono::seconds::period, std::chrono::seconds>(
+      std::chrono::seconds(s));
+}
+
+constexpr auto operator"" _min(unsigned long long int min) {
+  return SI::time_t<std::chrono::minutes::period, std::chrono::minutes>(
+      std::chrono::minutes(min));
 }
