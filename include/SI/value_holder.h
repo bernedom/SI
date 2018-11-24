156180e21cad4aebc462c016b3fc6ba6bb90b0b1
Dominik Berner <dominik.berner@gmail.com>
2018-11-24 15:28:39 +0100

    first implementation of same unit multiplication

--- include/SI/value_holder.h
+++ include/SI/value_holder.h
@@ -10,7 +10,8 @@
  * @tparam _Ratio Ratio in relation to the base unit of a SI unit
  * @tparam _Type internal representation of the value
  **/
-namespace SI {
+namespace SI
+{
 
 template <
     char _Symbol, class _Ratio = std::ratio<1>, typename _Type = long long int,
@@ -19,7 +20,8 @@ template <
         nullptr,
     typename std::enable_if<std::is_arithmetic<_Type>::value>::type * = nullptr>
 
-struct value_holder_t {
+struct value_holder_t
+{
 
   static_assert(detail::is_ratio<_Ratio>::value, "_Ratio is a std::ratio");
   typedef _Ratio ratio;
@@ -33,7 +35,8 @@ struct value_holder_t {
   template <class Rr = std::ratio<1>>
   constexpr bool
   operator==(const value_holder_t<symbol::value, Rr, internal_type,
-                                  exponent::value> &rhs) const {
+                                  exponent::value> &rhs) const
+  {
 
     return (rhs.value_ *
             std::remove_reference<decltype(rhs)>::type::ratio::num /
@@ -41,8 +44,21 @@ struct value_holder_t {
            (value_ * ratio::num / ratio::den);
   }
   /// multiply with a non-unit scalar
-  constexpr value_holder_t operator*(const _Type f) const {
+  constexpr value_holder_t operator*(const _Type f) const
+  {
     return {value_ * f};
+  }
+
+  /// multiply with a same unit
+  constexpr auto operator*(
+      const value_holder_t<symbol::value, ratio, internal_type, exponent::value>
+          &rhs) const
+  {
+
+    return value_holder_t<symbol::value, ratio, internal_type,
+                          exponent::value + std::remove_reference<decltype(
+                                                rhs)>::type::exponent::value>{
+        value_ * rhs.raw_value()};
   }
 
   /// negate operation
@@ -53,7 +69,8 @@ struct value_holder_t {
             typename std::enable_if<Rr::num == 1 || Rr::den == 1, Rr>::type * =
                 nullptr>
   constexpr auto ratio_to(const value_holder_t<symbol::value, Rr, internal_type,
-                                               exponent::value> &rhs) const {
+                                               exponent::value> &rhs) const
+  {
     typedef std::ratio_divide<Rr, ratio> resulting_ratio;
     return resulting_ratio{};
   }
