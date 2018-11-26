ee270dc905cb929d1eab8d693ed7be568a6256c2
Dominik Berner <dominik.berner@gmail.com>
2018-11-26 12:52:37 +0100

    Multiplication of units with different exponents works

--- test/src/area_tests.cc
+++ test/src/area_tests.cc
@@ -9,3 +9,18 @@ TEST_CASE("Check that exponent for area is 2") {
 
   static_assert(decltype(a)::exponent::value == 2, "area exponent is 2");
 }
+
+TEST_CASE("Check that length * lenght * length == exponent 3") {
+  constexpr auto l = 1_m;
+  constexpr auto a = l * l * l;
+
+  static_assert(decltype(a)::exponent::value == 3, "area exponent is 3");
+}
+
+TEST_CASE("Check that area * length == exponent 3") {
+  constexpr auto l = 1_m;
+  constexpr auto a = l * l;
+  constexpr auto v = a * l;
+
+  static_assert(decltype(v)::exponent::value == 3, "area exponent is 3");
+}
