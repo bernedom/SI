0f28a1bffe91c61c01086ba07c313e99b1320711
Dominik Berner <dominik.berner@gmail.com>
2018-11-27 21:56:43 +0100

    Removed debug operator

--- include/SI/length.h
+++ include/SI/length.h
@@ -5,63 +5,49 @@
 
 #include "value_holder.h"
 
-namespace SI
-{
+namespace SI {
 /**
  * @brief specialisation of value_holder_t to represent length units
  *
  * */
 template <class _Ratio = std::ratio<1>, typename _Type = long long int>
-struct length_t : public value_holder_t<'L', _Ratio, _Type>
-{
+struct length_t : public value_holder_t<'L', _Ratio, _Type> {
   using value_holder_t<'L', _Ratio, _Type>::value_holder_t;
 
   using value_holder_t<'L', _Ratio, _Type>::operator*;
 };
 
 } // namespace SI
-constexpr auto operator"" _ack(const unsigned long long int x)
-{
-  return SI::detail::generate_unit_type_overflow_check<SI::length_t,
-                                                       std::ratio<1>>(x);
-}
 
-constexpr auto operator"" _m(const unsigned long long int m)
-{
+constexpr auto operator"" _m(const unsigned long long int m) {
   return SI::detail::generate_unit_type_overflow_check<SI::length_t,
                                                        std::ratio<1>>(m);
 }
 
-constexpr auto operator"" _km(const unsigned long long int km)
-{
+constexpr auto operator"" _km(const unsigned long long int km) {
   return SI::detail::generate_unit_type_overflow_check<SI::length_t, std::kilo>(
       km);
 }
 
-constexpr auto operator"" _cm(const unsigned long long int cm)
-{
+constexpr auto operator"" _cm(const unsigned long long int cm) {
   return SI::detail::generate_unit_type_overflow_check<SI::length_t,
                                                        std::centi>(cm);
 }
 
-constexpr auto operator"" _mm(const unsigned long long int mm)
-{
+constexpr auto operator"" _mm(const unsigned long long int mm) {
   return SI::detail::generate_unit_type_overflow_check<SI::length_t,
                                                        std::milli>(mm);
 }
 
-constexpr auto operator"" _m(long double m)
-{
+constexpr auto operator"" _m(long double m) {
   return SI::length_t<std::ratio<1>, long double>(m);
 }
 
-constexpr auto operator"" _km(long double km)
-{
+constexpr auto operator"" _km(long double km) {
   return SI::length_t<std::kilo, long double>(km);
 }
 
-constexpr auto operator"" _mm(long double mm)
-{
+constexpr auto operator"" _mm(long double mm) {
   return SI::length_t<std::milli, long double>(mm);
 }
 // namespace SI
