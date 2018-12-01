1b0e70ee17cdb26cc98a93e311719cb83f7ec733
Dominik Berner <dominik.berner@gmail.com>
2018-12-01 15:50:39 +0100

    Signature for unit_cast added

--- test/src/unit_t_tests.cc
+++ test/src/unit_t_tests.cc
@@ -125,3 +125,14 @@ TEST_CASE("GIVEN a unit and a scalar WHEN scalar is divided by unit THEN unit "
     static_assert(decltype(result)::exponent::value == -1,
                   "Exponent is negative");
 }
+
+TEST_CASE("GIVEN two units of the same type and value WHEN cast from one to "
+          "another THEN units stay the same")
+{
+    constexpr unit_t<'X'> v1{0};
+
+    constexpr auto result = unit_cast<decltype(v1)>(v1);
+    static_assert(result == v1, "Result is same as input");
+    static_assert(std::is_same<decltype(result), decltype(v1)>::value,
+                  "casted type is the same");
+}
