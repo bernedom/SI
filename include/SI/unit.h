e59ed3750d4810e0e9caefc1ab864194e2c631b1
Dominik Berner <dominik.berner@gmail.com>
2018-12-01 16:28:41 +0100

    Documentation

--- include/SI/unit.h
+++ include/SI/unit.h
@@ -26,15 +26,21 @@ template <
 
 struct unit_t {
 
+  static_assert(_Exponent != 0, "Exponent is non-zero");
   static_assert(detail::is_ratio<_Ratio>::value, "_Ratio is a std::ratio");
   typedef _Ratio ratio;
   typedef _Type internal_type;
   typedef std::integral_constant<char, _Exponent> exponent;
   typedef std::integral_constant<char, _Symbol> symbol;
+
+  /// Construct with value v
   constexpr unit_t(_Type v) : value_{v} {}
 
+  /// returns the stored value as raw type
   constexpr internal_type raw_value() const { return value_; }
 
+  /// Comparison operator takes considers different ratios, i.e. 1000 micros ===
+  /// 1 milli
   template <class _rhs_Ratio = std::ratio<1>>
   constexpr bool
   operator==(const unit_t<symbol::value, exponent::value, _rhs_Ratio,
@@ -94,20 +100,29 @@ struct unit_t {
   internal_type value_;
 };
 
+/// operator to divide
+/// @todo return divided raw_value and handle different ratios
+template <char _Symbol>
+constexpr auto operator/(long long int, const unit_t<_Symbol> &rhs) {
+  return unit_t<_Symbol,
+                -std::remove_reference<decltype(rhs)>::type::exponent::value>{
+      0};
+}
+
+/// helper template to check if a type is a unit_t (false for all other types)
 template <typename _Tp> struct is_unit_t : std::false_type {};
 
+/// template specialisation to check if a type is a unit_t (true if unit_t)
 template <char _Symbol, char _Exponent, class _Ratio, typename _Type>
 struct is_unit_t<const unit_t<_Symbol, _Exponent, _Ratio, _Type>>
     : std::true_type {};
 
+/// non-const specialisation of check above
 template <char _Symbol, char _Exponent, class _Ratio, typename _Type>
 struct is_unit_t<unit_t<_Symbol, _Exponent, _Ratio, _Type>> : std::true_type {};
 
-template <char _Symbol>
-constexpr auto operator/(long long int, const unit_t<_Symbol> &rhs) {
-  return unit_t<_Symbol, -1>{0};
-}
-
+/// function to cast between two units of the same type
+/// @todo handle different ratios and return correct value
 template <typename _T, typename _rhs_T,
           typename std::enable_if<std::is_base_of<
               unit_t<_rhs_T::symbol::value, _rhs_T::exponent::value,
