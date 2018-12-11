2ebe77edb57d6bfd43dfaf8b96762b6d4c5d40db
Dominik Berner <dominik.berner@gmail.com>
2018-12-11 18:04:53 +0100

    Added operator+ to unit

--- include/SI/unit.h
+++ include/SI/unit.h
@@ -4,7 +4,8 @@
 #include <ratio>
 #include <type_traits>
 
-namespace SI {
+namespace SI
+{
 
 // forward declarations
 template <typename _T, typename _rhs_T>
@@ -28,122 +29,141 @@ template <
         nullptr,
     typename std::enable_if<std::is_arithmetic<_Type>::value>::type * = nullptr>
 
-struct unit_t {
+struct unit_t
+{
 
-  static_assert(_Exponent != 0, "Exponent is non-zero");
-  static_assert(detail::is_ratio<_Ratio>::value, "_Ratio is a std::ratio");
-  typedef _Ratio ratio;
-  typedef _Type internal_type;
-  typedef std::integral_constant<char, _Exponent> exponent;
-  typedef std::integral_constant<char, _Symbol> symbol;
+    static_assert(_Exponent != 0, "Exponent is non-zero");
+    static_assert(detail::is_ratio<_Ratio>::value, "_Ratio is a std::ratio");
+    typedef _Ratio ratio;
+    typedef _Type internal_type;
+    typedef std::integral_constant<char, _Exponent> exponent;
+    typedef std::integral_constant<char, _Symbol> symbol;
 
-  /// Construct with value v
-  constexpr unit_t(_Type v) : value_{v} {}
+    /// Construct with value v
+    constexpr unit_t(_Type v) : value_{v} {}
 
-  /// returns the stored value as raw type
-  constexpr internal_type raw_value() const { return value_; }
+    /// returns the stored value as raw type
+    constexpr internal_type raw_value() const { return value_; }
 
-  /// Comparison operator takes considers different ratios, i.e. 1000 micros ===
-  /// 1 milli
-  template <class _rhs_Ratio = std::ratio<1>>
-  constexpr bool
-  operator==(const unit_t<symbol::value, exponent::value, _rhs_Ratio,
-                          internal_type> &rhs) const {
-    typedef typename std::remove_reference<decltype(rhs)>::type rhs_t;
-    return unit_cast<unit_t<_Symbol, _Exponent, _Ratio, _Type>>(rhs)
-               .raw_value() == value_;
-  }
-  /// multiply with a non-unit scalar
-  constexpr unit_t operator*(const _Type f) const { return {value_ * f}; }
+    /// Comparison operator takes considers different ratios, i.e. 1000 micros ===
+    /// 1 milli
+    template <class _rhs_Ratio = std::ratio<1>>
+    constexpr bool
+    operator==(const unit_t<symbol::value, exponent::value, _rhs_Ratio,
+                            internal_type> &rhs) const
+    {
+        typedef typename std::remove_reference<decltype(rhs)>::type rhs_t;
+        return unit_cast<unit_t<_Symbol, _Exponent, _Ratio, _Type>>(rhs)
+                   .raw_value() == value_;
+    }
+    /// multiply with a non-unit scalar
+    constexpr unit_t operator*(const _Type f) const { return {value_ * f}; }
 
-  /// multiply with a same unit
-  /// resulting unit is the same as 'this'/left hand side of operation
-  /// @todo use unit_cast to get correct value
-  template <char _rhs_Exponent, typename _rhs_Ratio>
-  constexpr auto operator*(const unit_t<symbol::value, _rhs_Exponent,
-                                        _rhs_Ratio, internal_type> &rhs) const {
-    typedef typename std::remove_reference<decltype(rhs)>::type rhs_t;
-    constexpr auto conversion_ratio = detail::ratio_to<ratio, _rhs_Ratio>();
-    return unit_t<symbol::value, exponent::value + rhs_t::exponent::value,
-                  ratio, internal_type>{
-        value_ *
-        unit_cast<unit_t<_Symbol, _Exponent, _Ratio, _Type>>(rhs).raw_value()};
-  }
+    /// multiply with a same unit
+    /// resulting unit is the same as 'this'/left hand side of operation
+    /// @todo use unit_cast to get correct value
+    template <char _rhs_Exponent, typename _rhs_Ratio>
+    constexpr auto operator*(const unit_t<symbol::value, _rhs_Exponent,
+                                          _rhs_Ratio, internal_type> &rhs) const
+    {
+        typedef typename std::remove_reference<decltype(rhs)>::type rhs_t;
+        constexpr auto conversion_ratio = detail::ratio_to<ratio, _rhs_Ratio>();
+        return unit_t<symbol::value, exponent::value + rhs_t::exponent::value,
+                      ratio, internal_type>{
+            value_ *
+            unit_cast<unit_t<_Symbol, _Exponent, _Ratio, _Type>>(rhs).raw_value()};
+    }
 
-  /// multiply with a same unit
-  /// resulting unit is the same as 'this'/left hand side of operation
-  template <char _rhs_exponent, typename _rhs_Ratio,
-            typename std::enable_if<_rhs_exponent != exponent::value>::type * =
-                nullptr>
-  constexpr auto operator/(const unit_t<symbol::value, _rhs_exponent,
-                                        _rhs_Ratio, internal_type> &rhs) const {
-    typedef typename std::remove_reference<decltype(rhs)>::type rhs_t;
-    constexpr auto conversion_ratio = detail::ratio_to<ratio, _rhs_Ratio>();
+    /// multiply with a same unit
+    /// resulting unit is the same as 'this'/left hand side of operation
+    template <char _rhs_exponent, typename _rhs_Ratio,
+              typename std::enable_if<_rhs_exponent != exponent::value>::type * =
+                  nullptr>
+    constexpr auto operator/(const unit_t<symbol::value, _rhs_exponent,
+                                          _rhs_Ratio, internal_type> &rhs) const
+    {
+        typedef typename std::remove_reference<decltype(rhs)>::type rhs_t;
+        constexpr auto conversion_ratio = detail::ratio_to<ratio, _rhs_Ratio>();
 
-    return unit_t<symbol::value, exponent::value - rhs_t::exponent::value,
-                  ratio, internal_type>{
-        value_ /
-        unit_cast<unit_t<_Symbol, _Exponent, _Ratio, _Type>>(rhs).raw_value()};
-  }
+        return unit_t<symbol::value, exponent::value - rhs_t::exponent::value,
+                      ratio, internal_type>{
+            value_ /
+            unit_cast<unit_t<_Symbol, _Exponent, _Ratio, _Type>>(rhs).raw_value()};
+    }
 
-  /// if the same units of the same exponent are divided then the result is a
-  /// scalar
-  template <char _rhs_exponent, typename _rhs_Ratio,
-            typename std::enable_if<_rhs_exponent == exponent::value>::type * =
-                nullptr>
-  constexpr _Type
-  operator/(const unit_t<symbol::value, _rhs_exponent, _rhs_Ratio,
-                         internal_type> &rhs) const {
-    constexpr auto conversion_ratio = detail::ratio_to<ratio, _rhs_Ratio>();
+    /// if the same units of the same exponent are divided then the result is a
+    /// scalar
+    template <char _rhs_exponent, typename _rhs_Ratio,
+              typename std::enable_if<_rhs_exponent == exponent::value>::type * =
+                  nullptr>
+    constexpr _Type
+    operator/(const unit_t<symbol::value, _rhs_exponent, _rhs_Ratio,
+                           internal_type> &rhs) const
+    {
+        constexpr auto conversion_ratio = detail::ratio_to<ratio, _rhs_Ratio>();
 
-    return raw_value() / rhs.raw_value();
-  }
+        return raw_value() / rhs.raw_value();
+    }
 
-  /// negate operation
-  constexpr unit_t operator-() { return {-value_}; }
+    constexpr unit_t operator+(const unit_t &rhs) const
+    {
+        return unit_t{raw_value() + rhs.raw_value()};
+    }
 
-  internal_type value_;
+    /// negate operation
+    constexpr unit_t operator-() { return {-value_}; }
+
+    internal_type value_;
 };
 
 /// operator to divide
 /// @todo return divided raw_value and handle different ratios
 template <char _Symbol, char _Exponent, class _Ratio>
 constexpr auto operator/(long long int lhs,
-                         const unit_t<_Symbol, _Exponent, _Ratio> &rhs) {
+                         const unit_t<_Symbol, _Exponent, _Ratio> &rhs)
+{
 
-  return unit_cast<unit_t<_Symbol, -_Exponent, _Ratio>>(
-      unit_t<_Symbol, -_Exponent, std::ratio<1>>{
-          lhs / (rhs.raw_value() * (_Ratio::num / _Ratio::den))});
+    return unit_cast<unit_t<_Symbol, -_Exponent, _Ratio>>(
+        unit_t<_Symbol, -_Exponent, std::ratio<1>>{
+            lhs / (rhs.raw_value() * (_Ratio::num / _Ratio::den))});
 }
 
 /// helper template to check if a type is a unit_t (false for all other types)
-template <typename _Tp> struct is_unit_t : std::false_type {};
+template <typename _Tp>
+struct is_unit_t : std::false_type
+{
+};
 
 /// template specialisation to check if a type is a unit_t (true if unit_t)
 template <char _Symbol, char _Exponent, class _Ratio, typename _Type>
 struct is_unit_t<const unit_t<_Symbol, _Exponent, _Ratio, _Type>>
-    : std::true_type {};
+    : std::true_type
+{
+};
 
 /// non-const specialisation of check above
 template <char _Symbol, char _Exponent, class _Ratio, typename _Type>
-struct is_unit_t<unit_t<_Symbol, _Exponent, _Ratio, _Type>> : std::true_type {};
+struct is_unit_t<unit_t<_Symbol, _Exponent, _Ratio, _Type>> : std::true_type
+{
+};
 
 /// function to cast between two units of the same type
 template <typename _T, typename _rhs_T>
-constexpr auto unit_cast(const _rhs_T &rhs) {
-  // using static assert instead of std::enable if in order to be able to
-  // forward declare this function easier
-  static_assert(
-      is_unit_t<_rhs_T>::value ||
-          std::is_base_of<
-              unit_t<_rhs_T::symbol::value, _rhs_T::exponent::value,
-                     typename _rhs_T::ratio, typename _rhs_T::internal_type>,
-              _rhs_T>::value,
-      "is of type unit_t or a derived class");
-  constexpr auto conversion_ratio =
-      detail::ratio_to<typename _T::ratio, typename _rhs_T::ratio>();
-  return _T{(rhs.raw_value() * decltype(conversion_ratio)::num /
-             decltype(conversion_ratio)::den)};
+constexpr auto unit_cast(const _rhs_T &rhs)
+{
+    // using static assert instead of std::enable if in order to be able to
+    // forward declare this function easier
+    static_assert(
+        is_unit_t<_rhs_T>::value ||
+            std::is_base_of<
+                unit_t<_rhs_T::symbol::value, _rhs_T::exponent::value,
+                       typename _rhs_T::ratio, typename _rhs_T::internal_type>,
+                _rhs_T>::value,
+        "is of type unit_t or a derived class");
+    constexpr auto conversion_ratio =
+        detail::ratio_to<typename _T::ratio, typename _rhs_T::ratio>();
+    return _T{(rhs.raw_value() * decltype(conversion_ratio)::num /
+               decltype(conversion_ratio)::den)};
 }
 
 } // namespace SI
--- test/src/unit_t_operator_tests.cc
+++ test/src/unit_t_operator_tests.cc
@@ -6,8 +6,7 @@
 using namespace SI;
 
 TEST_CASE(
-    "given two values with exponent 1 WHEN multiplied THEN exponent is 2")
-{
+    "given two values with exponent 1 WHEN multiplied THEN exponent is 2") {
   constexpr unit_t<'X'> v1{0};
   constexpr unit_t<'X'> v2{0};
 
@@ -21,8 +20,7 @@ TEST_CASE(
 
 TEST_CASE("given two units with different non-negative values but same ratio "
           "and unit WHEN "
-          "multiplied THEN resulting value is correct")
-{
+          "multiplied THEN resulting value is correct") {
 
   constexpr unit_t<'X'> v1{20};
   constexpr unit_t<'X'> v2{30};
@@ -35,8 +33,7 @@ TEST_CASE("given two units with different non-negative values but same ratio "
 TEST_CASE(
     "given two units with different non-negative values and different ratio "
     "and unit WHEN "
-    "multiplied THEN resulting value is correct")
-{
+    "multiplied THEN resulting value is correct") {
 
   constexpr unit_t<'X', 1, std::deci> v1{2};
   constexpr unit_t<'X'> v2{30};
@@ -48,8 +45,7 @@ TEST_CASE(
 
 TEST_CASE(
     "given two units with different values and different ratio and unit WHEN "
-    "multiplied THEN resulting value is correct")
-{
+    "multiplied THEN resulting value is correct") {
 
   constexpr unit_t<'X', 1, std::deci> v1{2};
   constexpr unit_t<'X'> v2{-30};
@@ -64,8 +60,7 @@ TEST_CASE(
 
 TEST_CASE(
     "given two units with different values and ratio of rhs is small WHEN "
-    "multiplied THEN resulting type is of left hand side and value is 0")
-{
+    "multiplied THEN resulting type is of left hand side and value is 0") {
 
   constexpr unit_t<'X'> v1{2};
   constexpr unit_t<'X', 1, std::milli> v2{30};
@@ -79,8 +74,7 @@ TEST_CASE(
 }
 
 TEST_CASE("GIVEN two units with different exponents WHEN divided THEN "
-          "resulting exopnent is left exponent - right exponent")
-{
+          "resulting exopnent is left exponent - right exponent") {
   constexpr unit_t<'X', 3, std::ratio<1>> v1{1};
   constexpr unit_t<'X', 2, std::ratio<1>> v2{1};
 
@@ -91,8 +85,7 @@ TEST_CASE("GIVEN two units with different exponents WHEN divided THEN "
 }
 
 TEST_CASE(
-    "GIVEN two units with exponent 1 WHEN divided result in a raw integer")
-{
+    "GIVEN two units with exponent 1 WHEN divided result in a raw integer") {
   constexpr unit_t<'X', 1, std::ratio<1>> v1{1};
   constexpr auto result = v1 / v1;
 
@@ -103,8 +96,7 @@ TEST_CASE(
 
 TEST_CASE("GIVEN two units with the same ratio exponent 1  WHEN divided result "
           "is lhs.value / "
-          "rhs.value")
-{
+          "rhs.value") {
   constexpr unit_t<'X', 1, std::ratio<1>> v1{1000};
   constexpr unit_t<'X', 1, std::ratio<1>> v2{10};
   constexpr auto result = v1 / v2;
@@ -116,8 +108,7 @@ TEST_CASE("GIVEN two units with the same ratio exponent 1  WHEN divided result "
 }
 
 TEST_CASE("GIVEN a unit and a scalar WHEN scalar is divided by unit THEN unit "
-          "exponent is negative")
-{
+          "exponent is negative") {
   constexpr long long int v1{1};
   constexpr unit_t<'X', 1, std::ratio<1>> v2{1};
   constexpr auto result = v1 / v2;
@@ -127,8 +118,7 @@ TEST_CASE("GIVEN a unit and a scalar WHEN scalar is divided by unit THEN unit "
 }
 
 TEST_CASE("GIVEN a unit with ratio<1> and a scalar WHEN scalar is divided by "
-          "unit THEN resulting  value is scalar / unit.value ")
-{
+          "unit THEN resulting  value is scalar / unit.value ") {
   constexpr long long int v1{1000};
   constexpr unit_t<'X', 1, std::ratio<1>> v2{2};
   constexpr auto result = v1 / v2;
@@ -137,8 +127,7 @@ TEST_CASE("GIVEN a unit with ratio<1> and a scalar WHEN scalar is divided by "
 }
 
 TEST_CASE("GIVEN a unit with ratio<1, 1000> and a scalar WHEN scalar is dived "
-          "by unit THEN resulting value is adjusted by ratio")
-{
+          "by unit THEN resulting value is adjusted by ratio") {
   constexpr long long int v1{1000};
   constexpr unit_t<'X', 1, std::deca> v2{2};
 
@@ -148,4 +137,11 @@ TEST_CASE("GIVEN a unit with ratio<1, 1000> and a scalar WHEN scalar is dived "
   static_assert(std::ratio_equal<std::deca, decltype(result)::ratio>::value,
                 "Is of deca type");
   static_assert(result.raw_value() == 5, "1000 / 20 = 50");
+}
+
+TEST_CASE("GIVEN a unit  WHEN added to itself THEN resulting value value * 2") {
+  constexpr unit_t<'X', 1, std::ratio<1>> v1{1};
+  constexpr auto result = v1 + v1;
+
+  static_assert(result.raw_value() == 2, "Result is raw_value * 2");
 }
