cfb2798d96566b26c7cc8d53075d6621bc66572a
Dominik Berner <dominik.berner@gmail.com>
2019-01-09 12:35:19 +0100

    Refactored duplicate implementation out

--- include/SI/velocity.h
+++ include/SI/velocity.h
@@ -10,27 +10,15 @@ template <char _Exponent = 1, class _Ratio = std::ratio<1>,
           typename _Type = int64_t>
 using velocity = unit_t<'v', _Exponent, _Ratio, _Type>;
 
-template <typename _ratio_lhs, typename _ratio_rhs>
-constexpr auto operator/(const length_t<1, _ratio_lhs, int64_t> &lhs,
-                         const time_t<1, _ratio_rhs, int64_t> &rhs) {
+template <typename _ratio_lhs, typename _ratio_rhs, typename _type>
+constexpr auto operator/(const length_t<1, _ratio_lhs, _type> &lhs,
+                         const time_t<1, _ratio_rhs, _type> &rhs) {
   static_assert(detail::is_ratio<_ratio_lhs>::value &&
                     detail::is_ratio<_ratio_rhs>::value,
                 "template parametes are ratios");
 
-  return unit_cast<velocity<1, _ratio_lhs, int64_t>>(
-      velocity<1, std::ratio_multiply<_ratio_lhs, _ratio_rhs>, int64_t>{
-          lhs.raw_value() * rhs.raw_value()});
-}
-
-template <typename _ratio_lhs, typename _ratio_rhs>
-constexpr auto operator/(const length_t<1, _ratio_lhs, long double> &lhs,
-                         const time_t<1, _ratio_rhs, long double> &rhs) {
-  static_assert(detail::is_ratio<_ratio_lhs>::value &&
-                    detail::is_ratio<_ratio_rhs>::value,
-                "template parametes are ratios");
-
-  return unit_cast<velocity<1, _ratio_lhs, long double>>(
-      velocity<1, std::ratio_multiply<_ratio_lhs, _ratio_rhs>, long double>{
+  return unit_cast<velocity<1, _ratio_lhs, _type>>(
+      velocity<1, std::ratio_multiply<_ratio_lhs, _ratio_rhs>, _type>{
           lhs.raw_value() * rhs.raw_value()});
 }
 
