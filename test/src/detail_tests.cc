dc7a63b63ee4d14d232f6cb1fcf071f06ec2ca21
Dominik Berner <dominik.berner@gmail.com>
2019-01-31 07:14:28 +0100

    Added epsequal tests for NaN

--- test/src/detail_tests.cc
+++ test/src/detail_tests.cc
@@ -67,12 +67,45 @@ TEMPLATE_TEST_CASE("EpsEqual comparison of non_zero and epsilon/2 returns true",
   static_assert(SI::detail::epsEqual(half_eps, non_zero), "is equal");
 }
 
-TEMPLATE_TEST_CASE("EpsEqual comparison of non_zero and epsilon/2 returns true",
-                   "[detail][epsequal]", long double, double, float) {
-  constexpr TestType non_zero{std::numeric_limits<TestType>::NaN};
-  constexpr TestType half_eps{non_zero +
-                              std::numeric_limits<TestType>::epsilon() / 2};
+TEMPLATE_TEST_CASE(
+    "EpsEqual comparison of non_zero and quiet_NaN returns false",
+    "[detail][epsequal][runtime]", long double, double, float) {
+  constexpr TestType zero{0};
+  constexpr const TestType nan{std::numeric_limits<TestType>::quiet_NaN()};
 
-  static_assert(SI::detail::epsEqual(non_zero, half_eps), "is equal");
-  static_assert(SI::detail::epsEqual(half_eps, non_zero), "is equal");
+  REQUIRE(!SI::detail::epsEqual(zero, nan));
+  REQUIRE(!SI::detail::epsEqual(nan, zero));
+}
+
+TEMPLATE_TEST_CASE(
+    "EpsEqual comparison of quiet_NaN and quiet_NaN returns false",
+    "[detail][epsequal][runtime]", long double, double, float) {
+
+  constexpr const TestType nan{std::numeric_limits<TestType>::quiet_NaN()};
+
+  REQUIRE(!SI::detail::epsEqual(nan, nan));
+}
+
+TEMPLATE_TEST_CASE(
+    "EpsEqual comparison of signaling_NaN and quiet_NaN returns false",
+    "[detail][epsequal][runtime]", long double, double, float) {
+
+  constexpr const TestType signaling_nan{
+      std::numeric_limits<TestType>::signaling_NaN()};
+  constexpr const TestType quiet_nan{
+      std::numeric_limits<TestType>::quiet_NaN()};
+
+  REQUIRE(!SI::detail::epsEqual(signaling_nan, quiet_nan));
+}
+
+TEMPLATE_TEST_CASE(
+    "EpsEqual comparison of signaling_NaN and signaling_NaN returns false",
+    "[detail][epsequal][runtime]", long double, double, float) {
+
+  constexpr const TestType signaling_nan{
+      std::numeric_limits<TestType>::signaling_NaN()};
+  constexpr const TestType quiet_nan{
+      std::numeric_limits<TestType>::signaling_NaN()};
+
+  REQUIRE(!SI::detail::epsEqual(signaling_nan, quiet_nan));
 }
