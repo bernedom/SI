c8dee16ac4a5d8ada2b61f3baaa2c52369f1d1d2
Dominik Berner <dominik.berner@gmail.com>
2019-02-06 16:57:40 +0100

    Electric units have fixed exponents

--- include/SI/electric_charge.h
+++ include/SI/electric_charge.h
@@ -6,21 +6,20 @@
 
 namespace SI {
 
-template <char _Exponent = 1, class _Ratio = std::ratio<1>,
-          typename _Type = int64_t>
-using electric_charge_t = unit_t<'Q', _Exponent, _Ratio, _Type>;
+template <class _Ratio = std::ratio<1>, typename _Type = int64_t>
+using electric_charge_t = unit_t<'Q', 1, _Ratio, _Type>;
 
 /// multiply Amperes with seconds result is Coulomb
 /// @todo consider if this can be merged with unit_t::operator*
 template <typename _ratio_lhs, typename _ratio_rhs>
-constexpr auto operator*(const electric_current_t<1, _ratio_lhs> &lhs,
+constexpr auto operator*(const electric_current_t<_ratio_lhs> &lhs,
                          const time_t<1, _ratio_rhs> &rhs) {
   static_assert(detail::is_ratio<_ratio_lhs>::value &&
                     detail::is_ratio<_ratio_rhs>::value,
                 "template parametes are ratios");
 
-  return unit_cast<electric_charge_t<1, _ratio_lhs>>(
-      electric_charge_t<1, std::ratio_multiply<_ratio_lhs, _ratio_rhs>>{
+  return unit_cast<electric_charge_t<_ratio_lhs>>(
+      electric_charge_t<std::ratio_multiply<_ratio_lhs, _ratio_rhs>>{
           lhs.raw_value() * rhs.raw_value()});
 }
 
@@ -28,66 +27,64 @@ constexpr auto operator*(const electric_current_t<1, _ratio_lhs> &lhs,
 /// @todo get rid of double unit cast
 template <typename _ratio_lhs, typename _ratio_rhs>
 constexpr auto operator*(const time_t<1, _ratio_lhs> &lhs,
-                         const electric_current_t<1, _ratio_rhs> &rhs) {
+                         const electric_current_t<_ratio_rhs> &rhs) {
   static_assert(detail::is_ratio<_ratio_lhs>::value &&
                     detail::is_ratio<_ratio_rhs>::value,
                 "template parametes are ratios");
-  return unit_cast<electric_charge_t<1, _ratio_lhs>>(rhs * lhs);
+  return unit_cast<electric_charge_t<_ratio_lhs>>(rhs * lhs);
 }
 
 // divide charge with current, result is always seconds, regardless of ratios as
 // input
 template <typename _ratio_lhs, typename _ratio_rhs>
-constexpr auto operator/(const electric_charge_t<1, _ratio_lhs> &lhs,
-                         const electric_current_t<1, _ratio_rhs> &rhs) {
+constexpr auto operator/(const electric_charge_t<_ratio_lhs> &lhs,
+                         const electric_current_t<_ratio_rhs> &rhs) {
   return time_t<1>{
-      unit_cast<electric_charge_t<1, std::ratio<1>>>(lhs).raw_value() /
-      unit_cast<electric_current_t<1, std::ratio<1>>>(rhs).raw_value()};
+      unit_cast<electric_charge_t<std::ratio<1>>>(lhs).raw_value() /
+      unit_cast<electric_current_t<std::ratio<1>>>(rhs).raw_value()};
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs>
-constexpr auto operator/(const electric_charge_t<1, _ratio_lhs> &lhs,
+constexpr auto operator/(const electric_charge_t<_ratio_lhs> &lhs,
                          const time_t<1, _ratio_rhs> &rhs) {
-  return electric_current_t<1, std::ratio<1>>{
-      unit_cast<electric_charge_t<1, std::ratio<1>>>(lhs).raw_value() /
+  return electric_current_t<std::ratio<1>>{
+      unit_cast<electric_charge_t<std::ratio<1>>>(lhs).raw_value() /
       unit_cast<time_t<1, std::ratio<1>>>(rhs).raw_value()};
 }
 
 inline namespace literals {
 template <char... _Digits> constexpr auto operator""_mC() {
-  return SI::detail::check_overflow<electric_charge_t<1, std::milli>,
+  return SI::detail::check_overflow<electric_charge_t<std::milli>,
                                     _Digits...>();
 }
 
 template <char... _Digits> constexpr auto operator""_C() {
-  return SI::detail::check_overflow<electric_charge_t<1, std::ratio<1>>,
+  return SI::detail::check_overflow<electric_charge_t<std::ratio<1>>,
                                     _Digits...>();
 }
 
 template <char... _Digits> constexpr auto operator""_kC() {
-  return SI::detail::check_overflow<electric_charge_t<1, std::kilo>,
-                                    _Digits...>();
+  return SI::detail::check_overflow<electric_charge_t<std::kilo>, _Digits...>();
 }
 
 template <char... _Digits> constexpr auto operator""_MC() {
-  return SI::detail::check_overflow<electric_charge_t<1, std::mega>,
-                                    _Digits...>();
+  return SI::detail::check_overflow<electric_charge_t<std::mega>, _Digits...>();
 }
 
 constexpr auto operator"" _mC(long double mC) {
-  return electric_charge_t<1, std::milli, long double>(mC);
+  return electric_charge_t<std::milli, long double>(mC);
 }
 
 constexpr auto operator"" _C(long double C) {
-  return electric_charge_t<1, std::ratio<1>, long double>(C);
+  return electric_charge_t<std::ratio<1>, long double>(C);
 }
 
 constexpr auto operator"" _kC(long double kC) {
-  return electric_charge_t<1, std::kilo, long double>(kC);
+  return electric_charge_t<std::kilo, long double>(kC);
 }
 
 constexpr auto operator"" _MC(long double MC) {
-  return electric_charge_t<1, std::kilo, long double>(MC);
+  return electric_charge_t<std::kilo, long double>(MC);
 }
 
 } // namespace literals
--- include/SI/electric_current.h
+++ include/SI/electric_current.h
@@ -4,45 +4,44 @@
 
 namespace SI {
 
-template <char _Exponent = 1, class _Ratio = std::ratio<1>,
-          typename _Type = int64_t>
-using electric_current_t = unit_t<'I', _Exponent, _Ratio, _Type>;
+template <class _Ratio = std::ratio<1>, typename _Type = int64_t>
+using electric_current_t = unit_t<'I', 1, _Ratio, _Type>;
 
 inline namespace literals {
 template <char... _Digits> constexpr auto operator""_mA() {
-  return SI::detail::check_overflow<electric_current_t<1, std::milli>,
+  return SI::detail::check_overflow<electric_current_t<std::milli>,
                                     _Digits...>();
 }
 
 template <char... _Digits> constexpr auto operator""_A() {
-  return SI::detail::check_overflow<electric_current_t<1, std::ratio<1>>,
+  return SI::detail::check_overflow<electric_current_t<std::ratio<1>>,
                                     _Digits...>();
 }
 
 template <char... _Digits> constexpr auto operator""_kA() {
-  return SI::detail::check_overflow<electric_current_t<1, std::kilo>,
+  return SI::detail::check_overflow<electric_current_t<std::kilo>,
                                     _Digits...>();
 }
 
 template <char... _Digits> constexpr auto operator""_MA() {
-  return SI::detail::check_overflow<electric_current_t<1, std::mega>,
+  return SI::detail::check_overflow<electric_current_t<std::mega>,
                                     _Digits...>();
 }
 
 constexpr auto operator"" _mA(long double mA) {
-  return electric_current_t<1, std::milli, long double>(mA);
+  return electric_current_t<std::milli, long double>(mA);
 }
 
 constexpr auto operator"" _A(long double A) {
-  return electric_current_t<1, std::ratio<1>, long double>(A);
+  return electric_current_t<std::ratio<1>, long double>(A);
 }
 
 constexpr auto operator"" _kA(long double kA) {
-  return electric_current_t<1, std::kilo, long double>(kA);
+  return electric_current_t<std::kilo, long double>(kA);
 }
 
 constexpr auto operator"" _MA(long double MA) {
-  return electric_current_t<1, std::mega, long double>(MA);
+  return electric_current_t<std::mega, long double>(MA);
 }
 
 } // namespace literals
