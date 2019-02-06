fd575ed6fe2cfd20e3b667e5d19caaff88d49701
Dominik Berner <dominik.berner@gmail.com>
2019-02-06 16:53:05 +0100

    Added building acceleration from L/t^2

--- include/SI/acceleration.h
+++ include/SI/acceleration.h
@@ -18,4 +18,15 @@ constexpr auto operator/(const velocity_t<_ratio_lhs, _Type> &lhs,
       lhs.raw_value() / rhs.raw_value()};
 }
 
+template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
+constexpr auto operator/(const length_t<1, _ratio_lhs, _Type> &lhs,
+                         const time_t<2, _ratio_rhs, _Type> &rhs) {
+  static_assert(detail::is_ratio<_ratio_lhs>::value &&
+                    detail::is_ratio<_ratio_rhs>::value,
+                "template parametes are ratios");
+
+  return acceleration_t<std::ratio_divide<_ratio_lhs, _ratio_rhs>, _Type>{
+      lhs.raw_value() / rhs.raw_value()};
+}
+
 } // namespace SI
--- test/src/acceleration_tests.cc
+++ test/src/acceleration_tests.cc
@@ -5,13 +5,24 @@
 
 using namespace SI::literals;
 
-/// @todo add building from L / t^2
+/// @todo add decay to L, a and t&2
 
-TEST_CASE("GIVEN one velocity value WHEN divided by time value THEN result is "
+TEST_CASE("GIVEN velocity value WHEN divided by time value THEN result is "
           "an acceleration value") {
 
   constexpr SI::velocity_t<std::ratio<1>, int64_t> one_ms{1};
   constexpr auto acceleration = one_ms / 1_s;
+
+  static_assert(
+      std::is_same<decltype(acceleration),
+                   const SI::acceleration_t<std::ratio<1>, int64_t>>::value);
+}
+
+TEST_CASE("GIVEN a length value WHEN divided by time squared THEN result is an "
+          "acceleration value") {
+  constexpr auto one_m = 1_m;
+  constexpr auto one_square_s = 1_s * 1_s;
+  constexpr auto acceleration = one_m / one_square_s;
 
   static_assert(
       std::is_same<decltype(acceleration),
