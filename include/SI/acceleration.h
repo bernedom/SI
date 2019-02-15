08ee4f65f861fdb5638737ca65f84d982048383d
Dominik Berner <dominik.berner@gmail.com>
2019-02-15 16:10:21 +0100

    Added decay to length

--- include/SI/acceleration.h
+++ include/SI/acceleration.h
@@ -2,26 +2,35 @@
 
 #include "velocity.h"
 
-namespace SI
-{
+namespace SI {
 
 template <class _Ratio, typename _Type = int64_t>
 using acceleration_t = unit_t<'a', 2, _Ratio, _Type>;
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator/(const velocity_t<_ratio_lhs, _Type> &lhs,
-                         const time_t<1, _ratio_rhs, _Type> &rhs)
-{
+                         const time_t<1, _ratio_rhs, _Type> &rhs) {
 
   return detail::cross_unit_divide<acceleration_t>(lhs, rhs);
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator/(const length_t<_ratio_lhs, _Type> &lhs,
-                         const time_t<2, _ratio_rhs, _Type> &rhs)
-{
+                         const time_t<2, _ratio_rhs, _Type> &rhs) {
 
   return detail::cross_unit_divide<acceleration_t>(lhs, rhs);
+}
+
+template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
+constexpr auto operator*(const acceleration_t<_ratio_lhs, _Type> &lhs,
+                         const time_t<2, _ratio_rhs, _Type> &rhs) {
+  return detail::cross_unit_multiply<length_t>(lhs, rhs);
+}
+
+template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
+constexpr auto operator*(const time_t<2, _ratio_lhs, _Type> &lhs,
+                         const acceleration_t<_ratio_rhs, _Type> &rhs) {
+  return rhs * lhs;
 }
 
 } // namespace SI
--- test/src/acceleration_tests.cc
+++ test/src/acceleration_tests.cc
@@ -28,3 +28,19 @@ TEST_CASE("GIVEN a length value WHEN divided by time squared THEN result is an "
       std::is_same<decltype(acceleration),
                    const SI::acceleration_t<std::ratio<1>, int64_t>>::value);
 }
+
+TEST_CASE("GIVEN an acceleration value WHEN multiplied by time squared THEN "
+          "result is a length value") {
+
+  constexpr auto one_square_s = 1_s * 1_s;
+  constexpr auto acceleration = 1_m / one_square_s;
+  constexpr auto length = acceleration * one_square_s;
+  constexpr auto length_commutative = one_square_s * acceleration;
+
+  static_assert(
+      std::is_same<decltype(length),
+                   const SI::length_t<std::ratio<1>, int64_t>>::value);
+  static_assert(
+      std::is_same<decltype(length_commutative),
+                   const SI::length_t<std::ratio<1>, int64_t>>::value);
+}
