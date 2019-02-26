6f6b837b695c9be06ed9bed57841655e2311881c
Dominik Berner <dominik.berner@gmail.com>
2019-02-26 13:56:09 +0100

    Added comments for doyxgen

--- include/SI/acceleration.h
+++ include/SI/acceleration.h
@@ -2,72 +2,84 @@
 
 #include "velocity.h"
 
-namespace SI {
+namespace SI
+{
 
+/// Type for acceleration a = v / t or a = L / t^2
 template <class _Ratio, typename _Type = int64_t>
 using acceleration_t = unit_t<'a', 2, _Ratio, _Type>;
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator/(const velocity_t<_ratio_lhs, _Type> &lhs,
-                         const time_t<1, _ratio_rhs, _Type> &rhs) {
+                         const time_t<1, _ratio_rhs, _Type> &rhs)
+{
 
   return detail::cross_unit_divide<acceleration_t>(lhs, rhs);
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator/(const length_t<_ratio_lhs, _Type> &lhs,
-                         const time_t<2, _ratio_rhs, _Type> &rhs) {
+                         const time_t<2, _ratio_rhs, _Type> &rhs)
+{
 
   return detail::cross_unit_divide<acceleration_t>(lhs, rhs);
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator*(const acceleration_t<_ratio_lhs, _Type> &lhs,
-                         const time_t<2, _ratio_rhs, _Type> &rhs) {
+                         const time_t<2, _ratio_rhs, _Type> &rhs)
+{
   return detail::cross_unit_multiply<length_t>(lhs, rhs);
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator*(const time_t<2, _ratio_lhs, _Type> &lhs,
-                         const acceleration_t<_ratio_rhs, _Type> &rhs) {
+                         const acceleration_t<_ratio_rhs, _Type> &rhs)
+{
   return rhs * lhs;
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator*(const acceleration_t<_ratio_lhs, _Type> &lhs,
-                         const time_t<1, _ratio_rhs, _Type> &rhs) {
+                         const time_t<1, _ratio_rhs, _Type> &rhs)
+{
   return detail::cross_unit_multiply<velocity_t>(lhs, rhs);
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator*(const time_t<1, _ratio_lhs, _Type> &lhs,
-                         const acceleration_t<_ratio_rhs, _Type> &rhs) {
+                         const acceleration_t<_ratio_rhs, _Type> &rhs)
+{
   return rhs * lhs;
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator*(const acceleration_t<_ratio_lhs, _Type> &lhs,
-                         const velocity_t<_ratio_rhs, _Type> &rhs) {
+                         const velocity_t<_ratio_rhs, _Type> &rhs)
+{
 
   return detail::cross_unit_multiply<time_single_t>(lhs, rhs);
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator*(const velocity_t<_ratio_lhs, _Type> &lhs,
-                         const acceleration_t<_ratio_rhs, _Type> &rhs) {
+                         const acceleration_t<_ratio_rhs, _Type> &rhs)
+{
   return rhs * lhs;
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator*(const acceleration_t<_ratio_lhs, _Type> &lhs,
-                         const length_t<_ratio_rhs, _Type> &rhs) {
+                         const length_t<_ratio_rhs, _Type> &rhs)
+{
 
   return detail::cross_unit_multiply<time_squared_t>(lhs, rhs);
 }
 
 template <typename _ratio_lhs, typename _ratio_rhs, typename _Type = int64_t>
 constexpr auto operator*(const length_t<_ratio_lhs, _Type> &lhs,
-                         const acceleration_t<_ratio_rhs, _Type> &rhs) {
+                         const acceleration_t<_ratio_rhs, _Type> &rhs)
+{
   return rhs * lhs;
 }
 
--- include/SI/detail.h
+++ include/SI/detail.h
@@ -16,18 +16,23 @@ using Int_Parser = SI::detail::int_parse::_Parse_int<_Digits...>;
 
 #include "static_gcd.h"
 
-namespace SI::detail {
+/// Namespace containing implementation details for SI
+namespace SI::detail
+{
 
 /// Kudos to std::chrono authors for the overflow check of literals
 template <typename _Rep, unsigned long long _Val>
 struct _checked_integral_constant
-    : std::integral_constant<_Rep, static_cast<_Rep>(_Val)> {
+    : std::integral_constant<_Rep, static_cast<_Rep>(_Val)>
+{
   static_assert(_checked_integral_constant::value >= 0 &&
                     _checked_integral_constant::value == _Val,
                 "literal value cannot be represented by duration type");
 };
 
-template <typename _Unit, char... _Digits> constexpr _Unit check_overflow() {
+template <typename _Unit, char... _Digits>
+constexpr _Unit check_overflow()
+{
   using _Val = Int_Parser<_Digits...>;
   using _Rep = typename _Unit::internal_type;
   using _CheckedVal = _checked_integral_constant<_Rep, _Val::value>;
@@ -35,14 +40,20 @@ template <typename _Unit, char... _Digits> constexpr _Unit check_overflow() {
 }
 
 /// to check if a template is an instatiation of std::ratio
-template <typename _Tp> struct is_ratio : std::false_type {};
+template <typename _Tp>
+struct is_ratio : std::false_type
+{
+};
 
 template <intmax_t _Num, intmax_t _Den>
-struct is_ratio<std::ratio<_Num, _Den>> : std::true_type {};
+struct is_ratio<std::ratio<_Num, _Den>> : std::true_type
+{
+};
 
 template <typename T, typename std::enable_if<
                           std::is_floating_point<T>::value>::type * = nullptr>
-constexpr bool epsEqual(const T &lhs, const T &rhs) {
+constexpr bool epsEqual(const T &lhs, const T &rhs)
+{
 
   return (lhs - rhs) < std::numeric_limits<T>::epsilon() &&
          (lhs - rhs) > -std::numeric_limits<T>::epsilon();
@@ -53,7 +64,9 @@ constexpr bool epsEqual(const T &lhs, const T &rhs) {
 /// as described here
 /// https://stackoverflow.com/questions/36523038/stdcommon-type-trait-for-user-defined-types
 /// or as std::chrono does
-template <typename _ratio_lhs, typename _ratio_rhs> struct ratio_gcd {
+template <typename _ratio_lhs, typename _ratio_rhs>
+struct ratio_gcd
+{
 private:
   typedef static_gcd<_ratio_lhs::num, _ratio_rhs::num> gcd_num;
   typedef static_gcd<_ratio_lhs::den, _ratio_rhs::den> gcd_den;
