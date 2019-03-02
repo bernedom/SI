4562e0d144263f6eeb2fc08b68c4f771218eba80
Dominik Berner <dominik.berner@gmail.com>
2019-03-02 21:36:58 +0100

    Added macro to build unit from multiplication

--- include/SI/detail/operator_helpers.h
+++ include/SI/detail/operator_helpers.h
@@ -31,3 +31,30 @@
                            const RESULTING_UNIT_T<_ratio_rhs, _Type> &rhs) {   \
     return detail::cross_unit_divide<DIVISOR_UNIT_T>(lhs, rhs);                \
   }
+
+#define BUILD_UNIT_FROM_MULTIPLICATION(RESULTING_UNIT, UNIT_LHS, UNIT_RHS)     \
+                                                                               \
+  template <typename _ratio_lhs, typename _ratio_rhs, typename _Type>          \
+  constexpr auto operator*(const UNIT_LHS<_ratio_lhs, _Type> &lhs,             \
+                           const UNIT_RHS<_ratio_rhs, _Type> &rhs) {           \
+                                                                               \
+    return detail::cross_unit_multiply<RESULTING_UNIT>(lhs, rhs);              \
+  }                                                                            \
+                                                                               \
+  template <typename _ratio_lhs, typename _ratio_rhs, typename _Type>          \
+  constexpr auto operator*(const UNIT_RHS<_ratio_lhs, _Type> &lhs,             \
+                           const UNIT_LHS<_ratio_rhs, _Type> &rhs) {           \
+    return rhs * lhs;                                                          \
+  }                                                                            \
+                                                                               \
+  template <typename _ratio_lhs, typename _ratio_rhs, typename _Type>          \
+  constexpr auto operator/(const RESULTING_UNIT<_ratio_lhs, _Type> &lhs,       \
+                           const UNIT_LHS<_ratio_rhs, _Type> &rhs) {           \
+    return detail::cross_unit_divide<UNIT_RHS>(lhs, rhs);                      \
+  }                                                                            \
+                                                                               \
+  template <typename _ratio_lhs, typename _ratio_rhs, typename _Type>          \
+  constexpr auto operator/(const RESULTING_UNIT<_ratio_lhs, _Type> &lhs,       \
+                           const UNIT_RHS<_ratio_rhs, _Type> &rhs) {           \
+    return detail::cross_unit_divide<UNIT_LHS>(lhs, rhs);                      \
+  }
--- include/SI/electric_charge.h
+++ include/SI/electric_charge.h
@@ -13,38 +13,8 @@ namespace SI {
 template <class _Ratio = std::ratio<1>, typename _Type = int64_t>
 using electric_charge_t = unit_t<'Q', 1, _Ratio, _Type>;
 
-/// multiply Amperes with seconds result is Coulomb
-template <typename _ratio_lhs, typename _ratio_rhs>
-constexpr auto operator*(const electric_current_t<_ratio_lhs> &lhs,
-                         const time_t<1, _ratio_rhs> &rhs) {
-
-  return detail::cross_unit_multiply<electric_charge_t>(lhs, rhs);
-}
-
-/// multiply  seconds wit Amperes result is Coulomb
-template <typename _ratio_lhs, typename _ratio_rhs>
-constexpr auto operator*(const time_t<1, _ratio_lhs> &lhs,
-                         const electric_current_t<_ratio_rhs> &rhs) {
-  return rhs * lhs;
-}
-
-// divide charge with current, result is always seconds, regardless of ratios as
-// input
-template <typename _ratio_lhs, typename _ratio_rhs>
-constexpr auto operator/(const electric_charge_t<_ratio_lhs> &lhs,
-                         const electric_current_t<_ratio_rhs> &rhs) {
-  return time_t<1>{
-      unit_cast<electric_charge_t<std::ratio<1>>>(lhs).raw_value() /
-      unit_cast<electric_current_t<std::ratio<1>>>(rhs).raw_value()};
-}
-
-template <typename _ratio_lhs, typename _ratio_rhs>
-constexpr auto operator/(const electric_charge_t<_ratio_lhs> &lhs,
-                         const time_t<1, _ratio_rhs> &rhs) {
-  return electric_current_t<std::ratio<1>>{
-      unit_cast<electric_charge_t<std::ratio<1>>>(lhs).raw_value() /
-      unit_cast<time_t<1, std::ratio<1>>>(rhs).raw_value()};
-}
+BUILD_UNIT_FROM_MULTIPLICATION(electric_charge_t, electric_current_t,
+                               time_single_t)
 
 inline namespace literals {
 template <char... _Digits> constexpr auto operator""_mC() {
