1c1935d4559b062daad1444fe1ce8581776b6db3
Dominik Berner <dominik.berner@gmail.com>
2019-03-08 22:13:45 +0100

    Added Power structure

--- test/src/number_parser_tests.cc
+++ test/src/number_parser_tests.cc
@@ -21,15 +21,41 @@ template <intmax_t _base, char _Str_digit> struct Digit {
   static_assert(value < _base, "Digit is valid for base");
 };
 
+template <intmax_t _base, char _digit, char... _digits> struct Power_impl {
+
+  using recursive_power = Power_impl<_base, _digits...>;
+  static constexpr intmax_t power = recursive_power::power * _base;
+};
+
+// terminating case for power variadic template
+template <intmax_t _base, char _digit> struct Power_impl<_base, _digit> {
+  static constexpr intmax_t power = 1;
+};
+
+template <intmax_t _base, char... _digits>
+struct Power : Power_impl<_base, _digits...> {};
+
+template <intmax_t _base> struct Power<_base> {
+  static constexpr intmax_t power = 1;
+};
+
 template <intmax_t _power, char _digit, char... _digits> struct Number_impl {
   using digit = Digit<10, _digit>;
-  static constexpr intmax_t value = 10;
+
+  using recursive_number = Number_impl<_power, _digits...>;
+  static constexpr intmax_t value =
+      Digit<10, _digit>::value * _power + recursive_number::value;
+
+  static constexpr intmax_t magnitude = sizeof...(_digits);
+  static constexpr intmax_t power = Power<10, _digit, _digits...>::power;
 };
 
 // terminating case for variadic template
 template <intmax_t _power, char _digit> struct Number_impl<_power, _digit> {
   static_assert(_power == 1, "power should be one");
   static constexpr intmax_t value = Digit<10, _digit>::value;
+  static constexpr intmax_t magnitude = 0;
+  static constexpr intmax_t power = Power<10>::power;
 };
 
 template <char... _digits> struct Number : Number_impl<1, _digits...> {};
@@ -69,8 +95,47 @@ TEST_CASE(
   static_assert(number_9::value == 9);
 }
 
-TEST_CASE("GIVEN a two digit string WHEN passed to number structure THEN "
+TEST_CASE("GIVEN a single digit string WHEN passed to number structure THEN "
+          "magnitude is 1 ") {
+  using number_1 = Number<'1'>;
+  static_assert(number_1::magnitude == 0);
+
+  using number_9 = Number<'9'>;
+  static_assert(number_9::magnitude == 0);
+}
+
+TEST_CASE("GIVEN a single digit string WHEN passed to number structure THEN "
+          "power is 10^0 (1) ") {
+  using number_1 = Number<'1'>;
+  static_assert(number_1::power == 1);
+
+  using number_9 = Number<'9'>;
+  static_assert(number_9::power == 1);
+}
+
+TEST_CASE("GIVEN a multiple digit string WHEN passed to number structure THEN "
+          "magnitude is number of arguments - 1") {
+  using number_10 = Number<'1', '0'>;
+  static_assert(number_10::magnitude == 1);
+
+  using number_100 = Number<'1', '0', '0'>;
+  static_assert(number_100::magnitude == 2);
+}
+
+TEST_CASE("GIVEN a multiple digit string WHEN passed to number structure THEN"
+          "power is base^magnitude") {
+  using number_10 = Number<'1', '0'>;
+  static_assert(number_10::power == 10);
+
+  using number_100 = Number<'1', '0', '0'>;
+  static_assert(number_100::power == 10 * 10);
+}
+
+/*TEST_CASE("GIVEN a two digit string WHEN passed to number structure THEN "
           "stored value is an integer") {
   using number_10 = Number<'1', '0'>;
   static_assert(number_10::value == 10);
-}
+
+  using number_12 = Number<'1', '2'>;
+  static_assert(number_12::value == 12);
+}*/
