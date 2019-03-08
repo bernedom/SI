e57bc6463149e2f9a60e0ac4156eb4b0d876ecdd
Dominik Berner <dominik.berner@gmail.com>
2019-03-08 22:27:04 +0100

    Number implementation for base 10 works, no overflow check

--- test/src/number_parser_tests.cc
+++ test/src/number_parser_tests.cc
@@ -3,6 +3,7 @@
 #include <limits>
 #include <type_traits>
 
+/// struct converting a char digit into an int
 template <intmax_t _base, char _Str_digit> struct Digit {
   static_assert((_Str_digit >= '0' && _Str_digit <= '9') ||
                 (_Str_digit >= 'a' && _Str_digit <= 'f') ||
@@ -32,6 +33,7 @@ template <intmax_t _base, char _digit> struct Power_impl<_base, _digit> {
   static constexpr intmax_t power = 1;
 };
 
+// interface class for power calculation
 template <intmax_t _base, char... _digits>
 struct Power : Power_impl<_base, _digits...> {};
 
@@ -39,15 +41,16 @@ template <intmax_t _base> struct Power<_base> {
   static constexpr intmax_t power = 1;
 };
 
+// recursive struct that builds the number
 template <intmax_t _power, char _digit, char... _digits> struct Number_impl {
   using digit = Digit<10, _digit>;
 
-  using recursive_number = Number_impl<_power, _digits...>;
-  static constexpr intmax_t value =
-      Digit<10, _digit>::value * _power + recursive_number::value;
-
   static constexpr intmax_t magnitude = sizeof...(_digits);
   static constexpr intmax_t power = Power<10, _digit, _digits...>::power;
+
+  using recursive_number = Number_impl<_power, _digits...>;
+  static constexpr intmax_t value =
+      Digit<10, _digit>::value * power + recursive_number::value;
 };
 
 // terminating case for variadic template
@@ -58,6 +61,7 @@ template <intmax_t _power, char _digit> struct Number_impl<_power, _digit> {
   static constexpr intmax_t power = Power<10>::power;
 };
 
+/// interface class for number
 template <char... _digits> struct Number : Number_impl<1, _digits...> {};
 
 TEST_CASE("GIVEN a numeric value as string WHEN passed to a digit "
@@ -131,11 +135,20 @@ TEST_CASE("GIVEN a multiple digit string WHEN passed to number structure THEN"
   static_assert(number_100::power == 10 * 10);
 }
 
-/*TEST_CASE("GIVEN a two digit string WHEN passed to number structure THEN "
+TEST_CASE("GIVEN a two digit string WHEN passed to number structure THEN "
           "stored value is an integer") {
   using number_10 = Number<'1', '0'>;
   static_assert(number_10::value == 10);
 
   using number_12 = Number<'1', '2'>;
   static_assert(number_12::value == 12);
-}*/
+}
+
+TEST_CASE("GIVEN a three digit string WHEN passed to number structure THEN "
+          "stored value is an integer") {
+  using number_101 = Number<'1', '0', '1'>;
+  static_assert(number_101::value == 101);
+
+  using number_123 = Number<'1', '2', '3'>;
+  static_assert(number_123::value == 123);
+}
