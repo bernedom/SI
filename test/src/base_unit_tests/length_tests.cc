9a571d81ce9cce3a5314979248c3e2024214187b
Dominik Berner <dominik.berner@gmail.com>
2019-03-17 22:07:07 +0100

    Narrowed down tests for electric current and length

--- test/src/base_unit_tests/length_tests.cc
+++ test/src/base_unit_tests/length_tests.cc
@@ -4,12 +4,54 @@
 
 using namespace SI;
 
-/// @todo check tests for std::ratio_equal and resulting type
+TEST_CASE("GIVEN a value WHEN constructed with literal _m  THEN result is a "
+          "length type ratio 1 to 1 ") {
+
+  constexpr auto one = 1_m;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::length_t<int64_t, std::ratio<1>>>::value);
+  constexpr auto one_f = 1.0_m;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::ratio<1>>>::value);
+}
+
 TEST_CASE("GIVEN a value WHEN constructed with literal _km  THEN result is a "
           "length type ratio 1000 to 1") {
-  constexpr auto one_k = 1_km;
-  STATIC_REQUIRE(decltype(one_k)::ratio::num == 1000);
-  STATIC_REQUIRE(decltype(one_k)::ratio::den == 1);
+  constexpr auto one = 1_km;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::kilo>>::value);
+
+  constexpr auto one_f = 1.0_km;
+
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::kilo>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _mm  THEN result is a "
+          "length type ratio 1 to 1000") {
+
+  constexpr auto one = 1_mm;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::milli>>::value);
+  constexpr auto one_f = 1.0_mm;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::milli>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _cm  THEN result is a "
+          "length type ratio 1 to 100") {
+
+  constexpr auto one = 1_cm;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::centi>>::value);
+  constexpr auto one_f = 1.0_cm;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::centi>>::value);
 }
 
 TEST_CASE("GIVEN a value WHEN constructed with literal _km THEN it is one "
@@ -40,33 +82,6 @@ TEST_CASE("GIVEN a value of 1m WHEN multiplied with -1 THEN result is -1m") {
   constexpr auto neg_one_m = -1_m;
 
   STATIC_REQUIRE(one_m * -1 == neg_one_m);
-}
-
-TEST_CASE("GIVEN a value WHEN constructed with literal _km  THEN result is a "
-          "length type ratio 1000 to 1 floating point") {
-  constexpr auto one_k = 1.0_km;
-  STATIC_REQUIRE(decltype(one_k)::ratio::num == 1000);
-  STATIC_REQUIRE(decltype(one_k)::ratio::den == 1);
-}
-
-TEST_CASE("GIVEN a value WHEN constructed with literal _mm  THEN result is a "
-          "length type ratio 1 to 1000 floating point") {
-  constexpr auto one_mm = 1.0_mm;
-  STATIC_REQUIRE(decltype(one_mm)::ratio::num == 1);
-  STATIC_REQUIRE(decltype(one_mm)::ratio::den == 1000);
-}
-
-TEST_CASE("GIVEN a value WHEN constructed with literal _m  THEN result is a "
-          "length type ratio 1 to 1 floating point") {
-  constexpr auto one_m = 1.0_m;
-  STATIC_REQUIRE(decltype(one_m)::ratio::num == 1);
-  STATIC_REQUIRE(decltype(one_m)::ratio::den == 1);
-}
-
-TEST_CASE("GIVEN a value WHEN constructed with literal _cm  THEN result is a "
-          "length type ratio 1 to 100 floating point") {
-  constexpr auto one_cm = 1.0_cm;
-  STATIC_REQUIRE(std::ratio_equal<decltype(one_cm)::ratio, std::centi>::value);
 }
 
 /* Examples commented out fail to compile
