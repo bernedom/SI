f3ea924e0602103a4732b8ac03271993c9aedbfe
Dominik Berner <dominik.berner@gmail.com>
2019-03-21 22:03:07 +0100

    Removed unused functions to check overflow

--- include/SI/detail/detail.h
+++ include/SI/detail/detail.h
@@ -6,26 +6,13 @@
 #include <ratio>
 #include <stdexcept>
 #include <type_traits>
-template <char... _Digits>
-using Int_Parser = SI::detail::parsing::Number<_Digits...>;
 
 /// Namespace containing implementation details for SI
 namespace SI::detail {
 
-/// Checking for valid literals
-template <typename _Rep, unsigned long long _Val>
-struct _checked_integral_constant
-    : std::integral_constant<_Rep, static_cast<_Rep>(_Val)> {
-  static_assert(_checked_integral_constant::value >= 0 &&
-                    _checked_integral_constant::value == _Val,
-                "literal value cannot be represented by unit type");
-};
-
 template <typename _Unit, char... _Digits> constexpr _Unit check_overflow() {
-  using _Val = Int_Parser<_Digits...>;
-  using _Rep = typename _Unit::internal_type;
-  using _CheckedVal = _checked_integral_constant<_Rep, _Val::value>;
-  return _Unit{_CheckedVal::value};
+  // overflow check is handled inside inside number parser
+  return _Unit{SI::detail::parsing::Number<_Digits...>::value};
 }
 
 /// to check if a template is an instatiation of std::ratio
@@ -42,11 +29,6 @@ constexpr bool epsEqual(const T &lhs, const T &rhs) {
          (lhs - rhs) > -std::numeric_limits<T>::epsilon();
   // return std::abs(lhs - rhs) < std::numeric_limits<T>::epsilon();
 }
-
-/// @todo consider specializing std::common type
-/// as described here
-/// https://stackoverflow.com/questions/36523038/stdcommon-type-trait-for-user-defined-types
-/// or as std::chrono does
 template <typename _ratio_lhs, typename _ratio_rhs> struct ratio_gcd {
 private:
   using gcd_num = std::integral_constant<std::intmax_t,
