784bc35799c02ab2a37fca191fe8bcc9aa700be2
Dominik Berner <dominik.berner@gmail.com>
2019-03-22 23:07:14 +0100

    Added typedefs for luminosity

--- include/SI/luminosity.h
+++ include/SI/luminosity.h
@@ -6,32 +6,34 @@ namespace SI {
 template <typename _Type, typename _Ratio = std::ratio<1>>
 using luminosity_t = detail::unit_t<'J', 1, _Type, _Ratio>;
 
+template <typename _Type> using candela_t = luminosity_t<_Type, std::ratio<1>>;
+template <typename _Type>
+using milli_candela_t = luminosity_t<_Type, std::milli>;
+template <typename _Type> using kilo_candela_t = luminosity_t<_Type, std::kilo>;
+
 inline namespace literals {
 template <char... _Digits> constexpr auto operator""_cd() {
-  return SI::detail::check_overflow<luminosity_t<int64_t, std::ratio<1>>,
-                                    _Digits...>();
+  return SI::detail::check_overflow<candela_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator""_mcd() {
-  return SI::detail::check_overflow<luminosity_t<int64_t, std::milli>,
-                                    _Digits...>();
+template <char... _Digits> constexpr auto operator"" _kcd() {
+  return SI::detail::check_overflow<kilo_candela_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator""_kcd() {
-  return SI::detail::check_overflow<luminosity_t<int64_t, std::kilo>,
-                                    _Digits...>();
+template <char... _Digits> constexpr auto operator"" _mcd() {
+  return SI::detail::check_overflow<milli_candela_t<int64_t>, _Digits...>();
 }
 
 constexpr auto operator"" _cd(long double cd) {
-  return luminosity_t<long double, std::ratio<1>>(cd);
-}
-
-constexpr auto operator"" _mcd(long double mcd) {
-  return luminosity_t<long double, std::milli>(mcd);
+  return candela_t<long double>(cd);
 }
 
 constexpr auto operator"" _kcd(long double kcd) {
-  return luminosity_t<long double, std::kilo>(kcd);
+  return kilo_candela_t<long double>(kcd);
+}
+
+constexpr auto operator"" _mcd(long double mcd) {
+  return milli_candela_t<long double>(mcd);
 }
 
 } // namespace literals
