1e38a5b096a1e6d378df12409020f2d4a6734a5a
Dominik Berner <dominik.berner@gmail.com>
2019-03-22 23:11:40 +0100

    Added typedefs for substance

--- include/SI/substance.h
+++ include/SI/substance.h
@@ -3,35 +3,36 @@
 
 namespace SI {
 
-template <typename _Type, typename _Ratio = std::ratio<1>>
+template <typename _Type, typename _Ratio>
 using substance_t = detail::unit_t<'N', 1, _Type, _Ratio>;
 
+template <typename _Type> using mol_t = substance_t<_Type, std::ratio<1>>;
+template <typename _Type> using milli_mol_t = substance_t<_Type, std::milli>;
+template <typename _Type> using kilo_mol_t = substance_t<_Type, std::kilo>;
+
 inline namespace literals {
-template <char... _Dimolits> constexpr auto operator""_mol() {
-  return SI::detail::check_overflow<substance_t<int64_t, std::ratio<1>>,
-                                    _Dimolits...>();
+template <char... _Digits> constexpr auto operator""_mol() {
+  return SI::detail::check_overflow<mol_t<int64_t>, _Digits...>();
 }
 
-template <char... _Dimolits> constexpr auto operator""_mmol() {
-  return SI::detail::check_overflow<substance_t<int64_t, std::milli>,
-                                    _Dimolits...>();
+template <char... _Digits> constexpr auto operator"" _kmol() {
+  return SI::detail::check_overflow<kilo_mol_t<int64_t>, _Digits...>();
 }
 
-template <char... _Dimolits> constexpr auto operator""_kmol() {
-  return SI::detail::check_overflow<substance_t<int64_t, std::kilo>,
-                                    _Dimolits...>();
+template <char... _Digits> constexpr auto operator"" _mmol() {
+  return SI::detail::check_overflow<milli_mol_t<int64_t>, _Digits...>();
 }
 
 constexpr auto operator"" _mol(long double mol) {
-  return substance_t<long double, std::ratio<1>>(mol);
-}
-
-constexpr auto operator"" _mmol(long double mmol) {
-  return substance_t<long double, std::milli>(mmol);
+  return mol_t<long double>(mol);
 }
 
 constexpr auto operator"" _kmol(long double kmol) {
-  return substance_t<long double, std::kilo>(kmol);
+  return kilo_mol_t<long double>(kmol);
+}
+
+constexpr auto operator"" _mmol(long double mmol) {
+  return milli_mol_t<long double>(mmol);
 }
 
 } // namespace literals
