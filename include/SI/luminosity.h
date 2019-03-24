7f00ad7edb5582ff30539a3c04dd5ed8125c1229
Dominik Berner <dominik.berner@gmail.com>
2019-03-24 22:26:59 +0100

    Full metric prefixes for luminosity

--- include/SI/luminosity.h
+++ include/SI/luminosity.h
@@ -6,34 +6,127 @@ namespace SI {
 template <typename _Type, typename _Ratio>
 using luminosity_t = detail::unit_t<'J', 1, _Type, _Ratio>;
 
-template <typename _Type> using candela_t = luminosity_t<_Type, std::ratio<1>>;
+template <typename _Type> using atto_candea_t = luminosity_t<_Type, std::atto>;
 template <typename _Type>
-using milli_candela_t = luminosity_t<_Type, std::milli>;
-template <typename _Type> using kilo_candela_t = luminosity_t<_Type, std::kilo>;
+using femto_candea_t = luminosity_t<_Type, std::femto>;
+template <typename _Type> using pico_candea_t = luminosity_t<_Type, std::pico>;
+template <typename _Type> using nano_candea_t = luminosity_t<_Type, std::nano>;
+template <typename _Type>
+using micro_candea_t = luminosity_t<_Type, std::micro>;
+template <typename _Type>
+using milli_candea_t = luminosity_t<_Type, std::milli>;
+template <typename _Type> using candea_t = luminosity_t<_Type, std::ratio<1>>;
+template <typename _Type> using kilo_candea_t = luminosity_t<_Type, std::kilo>;
+template <typename _Type> using mega_candea_t = luminosity_t<_Type, std::mega>;
+template <typename _Type> using giga_candea_t = luminosity_t<_Type, std::giga>;
+template <typename _Type> using tera_candea_t = luminosity_t<_Type, std::tera>;
+template <typename _Type> using peta_candea_t = luminosity_t<_Type, std::peta>;
+template <typename _Type> using exa_candea_t = luminosity_t<_Type, std::exa>;
 
 inline namespace literals {
+
+template <char... _Digits> constexpr auto operator""_acd() {
+  return SI::detail::check_overflow<atto_candea_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_fcd() {
+  return SI::detail::check_overflow<femto_candea_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_pcd() {
+  return SI::detail::check_overflow<pico_candea_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_ncd() {
+  return SI::detail::check_overflow<nano_candea_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_ucd() {
+  return SI::detail::check_overflow<micro_candea_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_mcd() {
+  return SI::detail::check_overflow<milli_candea_t<int64_t>, _Digits...>();
+}
+
 template <char... _Digits> constexpr auto operator""_cd() {
-  return SI::detail::check_overflow<candela_t<int64_t>, _Digits...>();
+  return SI::detail::check_overflow<candea_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator"" _kcd() {
-  return SI::detail::check_overflow<kilo_candela_t<int64_t>, _Digits...>();
+template <char... _Digits> constexpr auto operator""_kcd() {
+  return SI::detail::check_overflow<kilo_candea_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator"" _mcd() {
-  return SI::detail::check_overflow<milli_candela_t<int64_t>, _Digits...>();
+template <char... _Digits> constexpr auto operator""_Mcd() {
+  return SI::detail::check_overflow<mega_candea_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _cd(long double cd) {
-  return candela_t<long double>(cd);
+template <char... _Digits> constexpr auto operator""_Gcd() {
+  return SI::detail::check_overflow<giga_candea_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _kcd(long double kcd) {
-  return kilo_candela_t<long double>(kcd);
+template <char... _Digits> constexpr auto operator""_Tcd() {
+  return SI::detail::check_overflow<tera_candea_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _mcd(long double mcd) {
-  return milli_candela_t<long double>(mcd);
+template <char... _Digits> constexpr auto operator""_Pcd() {
+  return SI::detail::check_overflow<peta_candea_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_Ecd() {
+  return SI::detail::check_overflow<exa_candea_t<int64_t>, _Digits...>();
+}
+
+constexpr auto operator""_acd(long double value) {
+  return atto_candea_t<long double>{value};
+}
+
+constexpr auto operator""_fcd(long double value) {
+  return femto_candea_t<long double>{value};
+}
+
+constexpr auto operator""_pcd(long double value) {
+  return pico_candea_t<long double>{value};
+}
+
+constexpr auto operator""_ncd(long double value) {
+  return nano_candea_t<long double>{value};
+}
+
+constexpr auto operator""_ucd(long double value) {
+  return micro_candea_t<long double>{value};
+}
+
+constexpr auto operator""_mcd(long double value) {
+  return milli_candea_t<long double>{value};
+}
+
+constexpr auto operator""_cd(long double value) {
+  return candea_t<long double>{value};
+}
+
+constexpr auto operator""_kcd(long double value) {
+  return kilo_candea_t<long double>{value};
+}
+
+constexpr auto operator""_Mcd(long double value) {
+  return mega_candea_t<long double>{value};
+}
+
+constexpr auto operator""_Gcd(long double value) {
+  return giga_candea_t<long double>{value};
+}
+
+constexpr auto operator""_Tcd(long double value) {
+  return tera_candea_t<long double>{value};
+}
+
+constexpr auto operator""_Pcd(long double value) {
+  return peta_candea_t<long double>{value};
+}
+
+constexpr auto operator""_Ecd(long double value) {
+  return exa_candea_t<long double>{value};
 }
 
 } // namespace literals
--- test/src/base_unit_tests/luminosity_tests.cc
+++ test/src/base_unit_tests/luminosity_tests.cc
@@ -4,17 +4,69 @@
 
 using namespace SI::literals;
 
-TEST_CASE("GIVEN a value WHEN constructed with literal _cd THEN result is a "
-          "luminosity type AND ratio 1 to 1") {
-  constexpr auto one = 1_cd;
+TEST_CASE("GIVEN a value WHEN constructed with literal _pcd THEN result is a "
+          "luminosity type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_acd;
   STATIC_REQUIRE(
       std::is_same<decltype(one),
-                   const SI::luminosity_t<int64_t, std::ratio<1>>>::value);
+                   const SI::luminosity_t<int64_t, std::atto>>::value);
 
-  constexpr auto one_f = 1.0_cd;
+  constexpr auto one_f = 1.0_acd;
   STATIC_REQUIRE(
       std::is_same<decltype(one_f),
-                   const SI::luminosity_t<long double, std::ratio<1>>>::value);
+                   const SI::luminosity_t<long double, std::atto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _pcd THEN result is a "
+          "luminosity type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_fcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::luminosity_t<int64_t, std::femto>>::value);
+
+  constexpr auto one_f = 1.0_fcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::luminosity_t<long double, std::femto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _pcd THEN result is a "
+          "luminosity type AND ratio 1 to 10^12") {
+  constexpr auto one = 1_pcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::luminosity_t<int64_t, std::pico>>::value);
+
+  constexpr auto one_f = 1.0_pcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::luminosity_t<long double, std::pico>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _ncd THEN result is a "
+          "luminosity type AND ratio 1 to 10^9") {
+  constexpr auto one = 1_ncd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::luminosity_t<int64_t, std::nano>>::value);
+
+  constexpr auto one_f = 1.0_ncd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::luminosity_t<long double, std::nano>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _ucd THEN result is a "
+          "luminosity type AND ratio 1 to 10^6") {
+  constexpr auto one = 1_ucd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::luminosity_t<int64_t, std::micro>>::value);
+
+  constexpr auto one_f = 1.0_ucd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::luminosity_t<long double, std::micro>>::value);
 }
 
 TEST_CASE("GIVEN a value WHEN constructed with literal _mcd THEN result is a "
@@ -30,6 +82,19 @@ TEST_CASE("GIVEN a value WHEN constructed with literal _mcd THEN result is a "
                    const SI::luminosity_t<long double, std::milli>>::value);
 }
 
+TEST_CASE("GIVEN a value WHEN constructed with literal _cd THEN result is a "
+          "luminosity type AND ratio 1 to 1") {
+  constexpr auto one = 1_cd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::luminosity_t<int64_t, std::ratio<1>>>::value);
+
+  constexpr auto one_f = 1.0_cd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::luminosity_t<long double, std::ratio<1>>>::value);
+}
+
 TEST_CASE("GIVEN a value WHEN constructed with literal _kcd THEN result is a "
           "luminosity type AND ratio 1000 to 1") {
   constexpr auto one = 1_kcd;
@@ -38,8 +103,72 @@ TEST_CASE("GIVEN a value WHEN constructed with literal _kcd THEN result is a "
                    const SI::luminosity_t<int64_t, std::kilo>>::value);
 
   constexpr auto one_f = 1.0_kcd;
-
   STATIC_REQUIRE(
       std::is_same<decltype(one_f),
                    const SI::luminosity_t<long double, std::kilo>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _Mcd THEN result is a "
+          "luminosity type AND ratio 10^6 to 1") {
+  constexpr auto one = 1_Mcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::luminosity_t<int64_t, std::mega>>::value);
+
+  constexpr auto one_f = 1.0_Mcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::luminosity_t<long double, std::mega>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _Gcd THEN result is a "
+          "luminosity type AND ratio 10^9 to 1") {
+  constexpr auto one = 1_Gcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::luminosity_t<int64_t, std::giga>>::value);
+
+  constexpr auto one_f = 1.0_Gcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::luminosity_t<long double, std::giga>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _Tcd THEN result is a "
+          "luminosity type AND ratio 10^12 to 1") {
+  constexpr auto one = 1_Tcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::luminosity_t<int64_t, std::tera>>::value);
+
+  constexpr auto one_f = 1.0_Tcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::luminosity_t<long double, std::tera>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _Pcd THEN result is a "
+          "luminosity type AND ratio 10^15 to 1") {
+  constexpr auto one = 1_Pcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::luminosity_t<int64_t, std::peta>>::value);
+
+  constexpr auto one_f = 1.0_Pcd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::luminosity_t<long double, std::peta>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _Ecd THEN result is a "
+          "luminosity type AND ratio 10^18 to 1") {
+  constexpr auto one = 1_Ecd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::luminosity_t<int64_t, std::exa>>::value);
+
+  constexpr auto one_f = 1.0_Ecd;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::luminosity_t<long double, std::exa>>::value);
 }
