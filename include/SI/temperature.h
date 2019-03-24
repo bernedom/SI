607bad319c392866e1f1d04269f19bb688399182
Dominik Berner <dominik.berner@gmail.com>
2019-03-24 22:32:23 +0100

    Full metric prefixes for temperature

--- include/SI/temperature.h
+++ include/SI/temperature.h
@@ -4,37 +4,130 @@
 namespace SI {
 
 /// @todo find a way to encapuslate unit symbol Theta
+/// @todo consider adding Celsius and fahrenheit
 template <typename _Type, typename _Ratio>
-using thermodynamic_temperature_t = detail::unit_t<'t', 1, _Type, _Ratio>;
-
+using temperature_t = detail::unit_t<'t', 1, _Type, _Ratio>;
+template <typename _Type> using atto_kelvin_t = temperature_t<_Type, std::atto>;
 template <typename _Type>
-using kelvin_t = thermodynamic_temperature_t<_Type, std::ratio<1>>;
+using femto_kelvin_t = temperature_t<_Type, std::femto>;
+template <typename _Type> using pico_kelvin_t = temperature_t<_Type, std::pico>;
+template <typename _Type> using nano_kelvin_t = temperature_t<_Type, std::nano>;
 template <typename _Type>
-using milli_kelvin_t = thermodynamic_temperature_t<_Type, std::milli>;
+using micro_kelvin_t = temperature_t<_Type, std::micro>;
 template <typename _Type>
-using kilo_kelvin_t = thermodynamic_temperature_t<_Type, std::kilo>;
+using milli_kelvin_t = temperature_t<_Type, std::milli>;
+template <typename _Type> using kelvin_t = temperature_t<_Type, std::ratio<1>>;
+template <typename _Type> using kilo_kelvin_t = temperature_t<_Type, std::kilo>;
+template <typename _Type> using mega_kelvin_t = temperature_t<_Type, std::mega>;
+template <typename _Type> using giga_kelvin_t = temperature_t<_Type, std::giga>;
+template <typename _Type> using tera_kelvin_t = temperature_t<_Type, std::tera>;
+template <typename _Type> using peta_kelvin_t = temperature_t<_Type, std::peta>;
+template <typename _Type> using exa_kelvin_t = temperature_t<_Type, std::exa>;
 
 inline namespace literals {
+
+template <char... _Digits> constexpr auto operator""_aK() {
+  return SI::detail::check_overflow<atto_kelvin_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_fK() {
+  return SI::detail::check_overflow<femto_kelvin_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_pK() {
+  return SI::detail::check_overflow<pico_kelvin_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_nK() {
+  return SI::detail::check_overflow<nano_kelvin_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_uK() {
+  return SI::detail::check_overflow<micro_kelvin_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_mK() {
+  return SI::detail::check_overflow<milli_kelvin_t<int64_t>, _Digits...>();
+}
+
 template <char... _Digits> constexpr auto operator""_K() {
   return SI::detail::check_overflow<kelvin_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator"" _kK() {
+template <char... _Digits> constexpr auto operator""_kK() {
   return SI::detail::check_overflow<kilo_kelvin_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator"" _mK() {
-  return SI::detail::check_overflow<milli_kelvin_t<int64_t>, _Digits...>();
+template <char... _Digits> constexpr auto operator""_MK() {
+  return SI::detail::check_overflow<mega_kelvin_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _K(long double K) { return kelvin_t<long double>(K); }
-
-constexpr auto operator"" _kK(long double kK) {
-  return kilo_kelvin_t<long double>(kK);
+template <char... _Digits> constexpr auto operator""_GK() {
+  return SI::detail::check_overflow<giga_kelvin_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _mK(long double mK) {
-  return milli_kelvin_t<long double>(mK);
+template <char... _Digits> constexpr auto operator""_TK() {
+  return SI::detail::check_overflow<tera_kelvin_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_PK() {
+  return SI::detail::check_overflow<peta_kelvin_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_EK() {
+  return SI::detail::check_overflow<exa_kelvin_t<int64_t>, _Digits...>();
+}
+
+constexpr auto operator""_aK(long double value) {
+  return atto_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_fK(long double value) {
+  return femto_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_pK(long double value) {
+  return pico_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_nK(long double value) {
+  return nano_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_uK(long double value) {
+  return micro_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_mK(long double value) {
+  return milli_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_K(long double value) {
+  return kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_kK(long double value) {
+  return kilo_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_MK(long double value) {
+  return mega_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_GK(long double value) {
+  return giga_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_TK(long double value) {
+  return tera_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_PK(long double value) {
+  return peta_kelvin_t<long double>{value};
+}
+
+constexpr auto operator""_EK(long double value) {
+  return exa_kelvin_t<long double>{value};
 }
 
 } // namespace literals
--- test/src/base_unit_tests/temperature_tests.cc
+++ test/src/base_unit_tests/temperature_tests.cc
@@ -4,41 +4,171 @@
 
 using namespace SI::literals;
 
-TEST_CASE("GIVEN a value WHEN constructed with literal _K THEN result is a "
-          "value of temperature type AND ratio 1 to 1") {
-  constexpr auto one = 1_K;
+TEST_CASE("GIVEN a value WHEN constructed with literal _pK THEN result is a "
+          "electric_current type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_aK;
   STATIC_REQUIRE(
-      std::is_same<decltype(one), const SI::thermodynamic_temperature_t<
-                                      int64_t, std::ratio<1>>>::value);
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::atto>>::value);
 
-  constexpr auto one_f = 1.0_K;
+  constexpr auto one_f = 1.0_aK;
   STATIC_REQUIRE(
-      std::is_same<decltype(one_f), const SI::thermodynamic_temperature_t<
-                                        long double, std::ratio<1>>>::value);
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::atto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _pK THEN result is a "
+          "electric_current type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_fK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::femto>>::value);
+
+  constexpr auto one_f = 1.0_fK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::femto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _pK THEN result is a "
+          "electric_current type AND ratio 1 to 10^12") {
+  constexpr auto one = 1_pK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::pico>>::value);
+
+  constexpr auto one_f = 1.0_pK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::pico>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _nK THEN result is a "
+          "electric_current type AND ratio 1 to 10^9") {
+  constexpr auto one = 1_nK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::nano>>::value);
+
+  constexpr auto one_f = 1.0_nK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::nano>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _uK THEN result is a "
+          "electric_current type AND ratio 1 to 10^6") {
+  constexpr auto one = 1_uK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::micro>>::value);
+
+  constexpr auto one_f = 1.0_uK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::micro>>::value);
 }
 
 TEST_CASE("GIVEN a value WHEN constructed with literal _mK THEN result is a "
-          "value of temperature type AND ratio 1 to 1000") {
+          "electric_current type AND ratio 1 to 1000") {
   constexpr auto one = 1_mK;
   STATIC_REQUIRE(
-      std::is_same<decltype(one), const SI::thermodynamic_temperature_t<
-                                      int64_t, std::milli>>::value);
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::milli>>::value);
 
   constexpr auto one_f = 1.0_mK;
   STATIC_REQUIRE(
-      std::is_same<decltype(one_f), const SI::thermodynamic_temperature_t<
-                                        long double, std::milli>>::value);
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::milli>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _K THEN result is a "
+          "electric_current type AND ratio 1 to 1") {
+  constexpr auto one = 1_K;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::ratio<1>>>::value);
+
+  constexpr auto one_f = 1.0_K;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::ratio<1>>>::value);
 }
 
 TEST_CASE("GIVEN a value WHEN constructed with literal _kK THEN result is a "
-          "value of temperature type ANDratio 1000 to 1") {
+          "electric_current type AND ratio 1000 to 1") {
   constexpr auto one = 1_kK;
   STATIC_REQUIRE(
-      std::is_same<decltype(one), const SI::thermodynamic_temperature_t<
-                                      int64_t, std::kilo>>::value);
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::kilo>>::value);
 
   constexpr auto one_f = 1.0_kK;
   STATIC_REQUIRE(
-      std::is_same<decltype(one_f), const SI::thermodynamic_temperature_t<
-                                        long double, std::kilo>>::value);
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::kilo>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _MK THEN result is a "
+          "electric_current type AND ratio 10^6 to 1") {
+  constexpr auto one = 1_MK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::mega>>::value);
+
+  constexpr auto one_f = 1.0_MK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::mega>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _GK THEN result is a "
+          "electric_current type AND ratio 10^9 to 1") {
+  constexpr auto one = 1_GK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::giga>>::value);
+
+  constexpr auto one_f = 1.0_GK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::giga>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _TK THEN result is a "
+          "electric_current type AND ratio 10^12 to 1") {
+  constexpr auto one = 1_TK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::tera>>::value);
+
+  constexpr auto one_f = 1.0_TK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::tera>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _PK THEN result is a "
+          "electric_current type AND ratio 10^15 to 1") {
+  constexpr auto one = 1_PK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::peta>>::value);
+
+  constexpr auto one_f = 1.0_PK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::peta>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _EK THEN result is a "
+          "electric_current type AND ratio 10^18 to 1") {
+  constexpr auto one = 1_EK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::temperature_t<int64_t, std::exa>>::value);
+
+  constexpr auto one_f = 1.0_EK;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::temperature_t<long double, std::exa>>::value);
 }
