e8c6020ab415160344fde3885f4816207c0196dc
Dominik Berner <dominik.berner@gmail.com>
2019-03-24 22:14:07 +0100

    Full metric prefixes for length

--- include/SI/length.h
+++ include/SI/length.h
@@ -6,44 +6,136 @@
 #include "detail/unit.h"
 
 namespace SI {
+/// @todo add astronomic units
 template <typename _Type, typename _Ratio>
 using length_t = detail::unit_t<'L', 1, _Type, _Ratio>;
 
-template <typename _Type> using meter_t = length_t<_Type, std::ratio<1>>;
+template <typename _Type> using atto_meter_t = length_t<_Type, std::atto>;
+template <typename _Type> using femto_meter_t = length_t<_Type, std::femto>;
+template <typename _Type> using pico_meter_t = length_t<_Type, std::pico>;
+template <typename _Type> using nano_meter_t = length_t<_Type, std::nano>;
+template <typename _Type> using micro_meter_t = length_t<_Type, std::micro>;
 template <typename _Type> using milli_meter_t = length_t<_Type, std::milli>;
 template <typename _Type> using centi_meter_t = length_t<_Type, std::centi>;
+template <typename _Type> using meter_t = length_t<_Type, std::ratio<1>>;
 template <typename _Type> using kilo_meter_t = length_t<_Type, std::kilo>;
+template <typename _Type> using mega_meter_t = length_t<_Type, std::mega>;
+template <typename _Type> using giga_meter_t = length_t<_Type, std::giga>;
+template <typename _Type> using tera_meter_t = length_t<_Type, std::tera>;
+template <typename _Type> using peta_meter_t = length_t<_Type, std::peta>;
+template <typename _Type> using exa_meter_t = length_t<_Type, std::exa>;
 
 inline namespace literals {
+template <char... _Digits> constexpr auto operator""_am() {
+  return SI::detail::check_overflow<atto_meter_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_fm() {
+  return SI::detail::check_overflow<femto_meter_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_pm() {
+  return SI::detail::check_overflow<pico_meter_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_nm() {
+  return SI::detail::check_overflow<nano_meter_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_um() {
+  return SI::detail::check_overflow<micro_meter_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_mm() {
+  return SI::detail::check_overflow<milli_meter_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_cm() {
+  return SI::detail::check_overflow<centi_meter_t<int64_t>, _Digits...>();
+}
 
 template <char... _Digits> constexpr auto operator""_m() {
   return SI::detail::check_overflow<meter_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator"" _km() {
+template <char... _Digits> constexpr auto operator""_km() {
   return SI::detail::check_overflow<kilo_meter_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator"" _cm() {
-  return SI::detail::check_overflow<centi_meter_t<int64_t>, _Digits...>();
+template <char... _Digits> constexpr auto operator""_Mm() {
+  return SI::detail::check_overflow<mega_meter_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator"" _mm() {
-  return SI::detail::check_overflow<milli_meter_t<int64_t>, _Digits...>();
+template <char... _Digits> constexpr auto operator""_Gm() {
+  return SI::detail::check_overflow<giga_meter_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _m(long double m) { return meter_t<long double>(m); }
-
-constexpr auto operator"" _km(long double km) {
-  return kilo_meter_t<long double>(km);
+template <char... _Digits> constexpr auto operator""_Tm() {
+  return SI::detail::check_overflow<tera_meter_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _cm(long double cm) {
-  return centi_meter_t<long double>(cm);
+template <char... _Digits> constexpr auto operator""_Pm() {
+  return SI::detail::check_overflow<peta_meter_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _mm(long double mm) {
-  return milli_meter_t<long double>(mm);
+template <char... _Digits> constexpr auto operator""_Em() {
+  return SI::detail::check_overflow<exa_meter_t<int64_t>, _Digits...>();
+}
+
+constexpr auto operator""_am(long double value) {
+  return atto_meter_t<long double>{value};
+}
+
+constexpr auto operator""_fm(long double value) {
+  return femto_meter_t<long double>{value};
+}
+
+constexpr auto operator""_pm(long double value) {
+  return pico_meter_t<long double>{value};
+}
+
+constexpr auto operator""_nm(long double value) {
+  return nano_meter_t<long double>{value};
+}
+
+constexpr auto operator""_um(long double value) {
+  return micro_meter_t<long double>{value};
+}
+
+constexpr auto operator""_mm(long double value) {
+  return milli_meter_t<long double>{value};
+}
+
+constexpr auto operator""_cm(long double value) {
+  return centi_meter_t<long double>{value};
+}
+
+constexpr auto operator""_m(long double value) {
+  return meter_t<long double>{value};
+}
+
+constexpr auto operator""_km(long double value) {
+  return kilo_meter_t<long double>{value};
+}
+
+constexpr auto operator""_Mm(long double value) {
+  return mega_meter_t<long double>{value};
+}
+
+constexpr auto operator""_Gm(long double value) {
+  return giga_meter_t<long double>{value};
+}
+
+constexpr auto operator""_Tm(long double value) {
+  return tera_meter_t<long double>{value};
+}
+
+constexpr auto operator""_Pm(long double value) {
+  return peta_meter_t<long double>{value};
+}
+
+constexpr auto operator""_Em(long double value) {
+  return exa_meter_t<long double>{value};
 }
 } // namespace literals
 } // namespace SI
--- test/src/base_unit_tests/length_tests.cc
+++ test/src/base_unit_tests/length_tests.cc
@@ -4,38 +4,72 @@
 
 using namespace SI;
 
-TEST_CASE("GIVEN a value WHEN constructed with literal _m  THEN result is a "
-          "length type ratio 1 to 1 ") {
-
-  constexpr auto one = 1_m;
-  STATIC_REQUIRE(
-      std::is_same<decltype(one),
-                   const SI::length_t<int64_t, std::ratio<1>>>::value);
-  constexpr auto one_f = 1.0_m;
-  STATIC_REQUIRE(
-      std::is_same<decltype(one_f),
-                   const SI::length_t<long double, std::ratio<1>>>::value);
-}
-
-TEST_CASE("GIVEN a value WHEN constructed with literal _km  THEN result is a "
-          "length type ratio 1000 to 1") {
-  constexpr auto one = 1_km;
+TEST_CASE("GIVEN a value WHEN constructed with literal _pm THEN result is a "
+          "length type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_am;
   STATIC_REQUIRE(std::is_same<decltype(one),
-                              const SI::length_t<int64_t, std::kilo>>::value);
+                              const SI::length_t<int64_t, std::atto>>::value);
 
-  constexpr auto one_f = 1.0_km;
-
+  constexpr auto one_f = 1.0_am;
   STATIC_REQUIRE(
       std::is_same<decltype(one_f),
-                   const SI::length_t<long double, std::kilo>>::value);
+                   const SI::length_t<long double, std::atto>>::value);
 }
 
-TEST_CASE("GIVEN a value WHEN constructed with literal _mm  THEN result is a "
-          "length type ratio 1 to 1000") {
+TEST_CASE("GIVEN a value WHEN constructed with literal _pm THEN result is a "
+          "length type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_fm;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::femto>>::value);
 
+  constexpr auto one_f = 1.0_fm;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::femto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _pm THEN result is a "
+          "length type AND ratio 1 to 10^12") {
+  constexpr auto one = 1_pm;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::pico>>::value);
+
+  constexpr auto one_f = 1.0_pm;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::pico>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _nm THEN result is a "
+          "length type AND ratio 1 to 10^9") {
+  constexpr auto one = 1_nm;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::nano>>::value);
+
+  constexpr auto one_f = 1.0_nm;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::nano>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _um THEN result is a "
+          "length type AND ratio 1 to 10^6") {
+  constexpr auto one = 1_um;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::micro>>::value);
+
+  constexpr auto one_f = 1.0_um;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::micro>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _mm THEN result is a "
+          "length type AND ratio 1 to 1000") {
   constexpr auto one = 1_mm;
   STATIC_REQUIRE(std::is_same<decltype(one),
                               const SI::length_t<int64_t, std::milli>>::value);
+
   constexpr auto one_f = 1.0_mm;
   STATIC_REQUIRE(
       std::is_same<decltype(one_f),
@@ -52,6 +86,91 @@ TEST_CASE("GIVEN a value WHEN constructed with literal _cm  THEN result is a "
   STATIC_REQUIRE(
       std::is_same<decltype(one_f),
                    const SI::length_t<long double, std::centi>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _m THEN result is a "
+          "length type AND ratio 1 to 1") {
+  constexpr auto one = 1_m;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::length_t<int64_t, std::ratio<1>>>::value);
+
+  constexpr auto one_f = 1.0_m;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::ratio<1>>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _km THEN result is a "
+          "length type AND ratio 1000 to 1") {
+  constexpr auto one = 1_km;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::kilo>>::value);
+
+  constexpr auto one_f = 1.0_km;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::kilo>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _Mm THEN result is a "
+          "length type AND ratio 10^6 to 1") {
+  constexpr auto one = 1_Mm;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::mega>>::value);
+
+  constexpr auto one_f = 1.0_Mm;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::mega>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _Gm THEN result is a "
+          "length type AND ratio 10^9 to 1") {
+  constexpr auto one = 1_Gm;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::giga>>::value);
+
+  constexpr auto one_f = 1.0_Gm;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::giga>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _Tm THEN result is a "
+          "length type AND ratio 10^12 to 1") {
+  constexpr auto one = 1_Tm;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::tera>>::value);
+
+  constexpr auto one_f = 1.0_Tm;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::tera>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _Pm THEN result is a "
+          "length type AND ratio 10^15 to 1") {
+  constexpr auto one = 1_Pm;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::peta>>::value);
+
+  constexpr auto one_f = 1.0_Pm;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::peta>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _Em THEN result is a "
+          "length type AND ratio 10^18 to 1") {
+  constexpr auto one = 1_Em;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::length_t<int64_t, std::exa>>::value);
+
+  constexpr auto one_f = 1.0_Em;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::length_t<long double, std::exa>>::value);
 }
 
 TEST_CASE("GIVEN a value WHEN constructed with literal _km THEN it is one "
@@ -83,17 +202,3 @@ TEST_CASE("GIVEN a value of 1m WHEN multiplied with -1 THEN result is -1m") {
 
   STATIC_REQUIRE(one_m * -1 == neg_one_m);
 }
-
-/* Examples commented out fail to compile
-TEST_CASE("Assert on overflow for
-m") { REQUIRE_THROWS(0xffffffffffffffff_m); } TEST_CASE("GIVEN a value WHEN
-constructed with literal _Assert on overflow for km") {
-  REQUIRE_THROWS(0xffffffffffffffff_km);
-}
-TEST_CASE("_Assert on overflow for
-cm") { REQUIRE_THROWS(0xffffffffffffffff_cm);
-}
-TEST_CASE("_Assert on overflow for
-mm") { REQUIRE_THROWS(0xffffffffffffffff_mm);
-}
-*/
