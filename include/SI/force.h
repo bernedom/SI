d2e798b7feca0ce89dd0efd215a98a5dae405b58
Dominik Berner <dominik.berner@gmail.com>
2019-03-24 22:44:52 +0100

    Full metric prefixes for force

--- include/SI/force.h
+++ include/SI/force.h
@@ -9,36 +9,129 @@ namespace SI {
 template <typename _Type, class _Ratio = std::ratio<1>>
 using force_t = detail::unit_t<'F', 1, _Type, _Ratio>;
 
-template <typename _Type> using newton_t = force_t<_Type, std::ratio<1>>;
+/// specific units
+template <typename _Type> using atto_newton_t = force_t<_Type, std::atto>;
+template <typename _Type> using femto_newton_t = force_t<_Type, std::femto>;
+template <typename _Type> using pico_newton_t = force_t<_Type, std::pico>;
+template <typename _Type> using nano_newton_t = force_t<_Type, std::nano>;
+template <typename _Type> using micro_newton_t = force_t<_Type, std::micro>;
 template <typename _Type> using milli_newton_t = force_t<_Type, std::milli>;
+template <typename _Type> using newton_t = force_t<_Type, std::ratio<1>>;
 template <typename _Type> using kilo_newton_t = force_t<_Type, std::kilo>;
+template <typename _Type> using mega_newton_t = force_t<_Type, std::mega>;
+template <typename _Type> using giga_newton_t = force_t<_Type, std::giga>;
+template <typename _Type> using tera_newton_t = force_t<_Type, std::tera>;
+template <typename _Type> using peta_newton_t = force_t<_Type, std::peta>;
+template <typename _Type> using exa_newton_t = force_t<_Type, std::exa>;
 
 namespace detail {
 BUILD_UNIT_FROM_MULTIPLICATION(force_t, mass_t, acceleration_t)
 }
 
 inline namespace literals {
+template <char... _Digits> constexpr auto operator""_aN() {
+  return SI::detail::check_overflow<atto_newton_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_fN() {
+  return SI::detail::check_overflow<femto_newton_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_pN() {
+  return SI::detail::check_overflow<pico_newton_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_nN() {
+  return SI::detail::check_overflow<nano_newton_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_uN() {
+  return SI::detail::check_overflow<micro_newton_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_mN() {
+  return SI::detail::check_overflow<milli_newton_t<int64_t>, _Digits...>();
+}
 
 template <char... _Digits> constexpr auto operator""_N() {
   return SI::detail::check_overflow<newton_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator"" _kN() {
+template <char... _Digits> constexpr auto operator""_kN() {
   return SI::detail::check_overflow<kilo_newton_t<int64_t>, _Digits...>();
 }
 
-template <char... _Digits> constexpr auto operator"" _mN() {
-  return SI::detail::check_overflow<milli_newton_t<int64_t>, _Digits...>();
+template <char... _Digits> constexpr auto operator""_MN() {
+  return SI::detail::check_overflow<mega_newton_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _N(long double N) { return newton_t<long double>(N); }
-
-constexpr auto operator"" _kN(long double kN) {
-  return kilo_newton_t<long double>(kN);
+template <char... _Digits> constexpr auto operator""_GN() {
+  return SI::detail::check_overflow<giga_newton_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _mN(long double mN) {
-  return milli_newton_t<long double>(mN);
+template <char... _Digits> constexpr auto operator""_TN() {
+  return SI::detail::check_overflow<tera_newton_t<int64_t>, _Digits...>();
 }
+
+template <char... _Digits> constexpr auto operator""_PN() {
+  return SI::detail::check_overflow<peta_newton_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_EN() {
+  return SI::detail::check_overflow<exa_newton_t<int64_t>, _Digits...>();
+}
+
+constexpr auto operator""_aN(long double value) {
+  return atto_newton_t<long double>{value};
+}
+
+constexpr auto operator""_fN(long double value) {
+  return femto_newton_t<long double>{value};
+}
+
+constexpr auto operator""_pN(long double value) {
+  return pico_newton_t<long double>{value};
+}
+
+constexpr auto operator""_nN(long double value) {
+  return nano_newton_t<long double>{value};
+}
+
+constexpr auto operator""_uN(long double value) {
+  return micro_newton_t<long double>{value};
+}
+
+constexpr auto operator""_mN(long double value) {
+  return milli_newton_t<long double>{value};
+}
+
+constexpr auto operator""_N(long double value) {
+  return newton_t<long double>{value};
+}
+
+constexpr auto operator""_kN(long double value) {
+  return kilo_newton_t<long double>{value};
+}
+
+constexpr auto operator""_MN(long double value) {
+  return mega_newton_t<long double>{value};
+}
+
+constexpr auto operator""_GN(long double value) {
+  return giga_newton_t<long double>{value};
+}
+
+constexpr auto operator""_TN(long double value) {
+  return tera_newton_t<long double>{value};
+}
+
+constexpr auto operator""_PN(long double value) {
+  return peta_newton_t<long double>{value};
+}
+
+constexpr auto operator""_EN(long double value) {
+  return exa_newton_t<long double>{value};
+}
+
 } // namespace literals
 } // namespace SI
--- test/src/derived_unit_tests/force_tests.cc
+++ test/src/derived_unit_tests/force_tests.cc
@@ -5,24 +5,68 @@
 #include <SI/mass.h>
 
 using namespace SI::literals;
+TEST_CASE("GIVEN a value WHEN constructed with literal _aN THEN result is a "
+          "force type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_aN;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::force_t<int64_t, std::atto>>::value);
 
-TEST_CASE("GIVEN a value WHEN constructed with literal _N; THEN result is a "
-          "force value AND ratio is 1") {
-  constexpr auto one = 1_N;
-
-  STATIC_REQUIRE(
-      std::is_same<decltype(one),
-                   const SI::force_t<int64_t, std::ratio<1>>>::value);
-
-  constexpr auto one_f = 1.0_N;
-
+  constexpr auto one_f = 1.0_aN;
   STATIC_REQUIRE(
       std::is_same<decltype(one_f),
-                   const SI::force_t<long double, std::ratio<1>>>::value);
+                   const SI::force_t<long double, std::atto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _fN THEN result is a "
+          "force type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_fN;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::force_t<int64_t, std::femto>>::value);
+
+  constexpr auto one_f = 1.0_fN;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::force_t<long double, std::femto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _pN THEN result is a "
+          "force type AND ratio 1 to 10^12") {
+  constexpr auto one = 1_pN;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::force_t<int64_t, std::pico>>::value);
+
+  constexpr auto one_f = 1.0_pN;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::force_t<long double, std::pico>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _nN THEN result is a "
+          "force type AND ratio 1 to 10^9") {
+  constexpr auto one = 1_nN;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::force_t<int64_t, std::nano>>::value);
+
+  constexpr auto one_f = 1.0_nN;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::force_t<long double, std::nano>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _uN THEN result is a "
+          "force type AND ratio 1 to 10^6") {
+  constexpr auto one = 1_uN;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::force_t<int64_t, std::micro>>::value);
+
+  constexpr auto one_f = 1.0_uN;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::force_t<long double, std::micro>>::value);
 }
 
 TEST_CASE("GIVEN a value WHEN constructed with literal _mN THEN result is a "
-          "force value AND ratio is 1/1000") {
+          "force type AND ratio 1 to 1000") {
   constexpr auto one = 1_mN;
   STATIC_REQUIRE(std::is_same<decltype(one),
                               const SI::force_t<int64_t, std::milli>>::value);
@@ -33,8 +77,21 @@ TEST_CASE("GIVEN a value WHEN constructed with literal _mN THEN result is a "
                    const SI::force_t<long double, std::milli>>::value);
 }
 
+TEST_CASE("GIVEN a value WHEN constructed with literal _N THEN result is a "
+          "force type AND ratio 1 to 1") {
+  constexpr auto one = 1_N;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::force_t<int64_t, std::ratio<1>>>::value);
+
+  constexpr auto one_f = 1.0_N;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::force_t<long double, std::ratio<1>>>::value);
+}
+
 TEST_CASE("GIVEN a value WHEN constructed with literal _kN THEN result is a "
-          "force value AND ratio is 1000/1") {
+          "force type AND ratio 1000 to 1") {
   constexpr auto one = 1_kN;
   STATIC_REQUIRE(std::is_same<decltype(one),
                               const SI::force_t<int64_t, std::kilo>>::value);
@@ -43,6 +100,65 @@ TEST_CASE("GIVEN a value WHEN constructed with literal _kN THEN result is a "
   STATIC_REQUIRE(
       std::is_same<decltype(one_f),
                    const SI::force_t<long double, std::kilo>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _MN THEN result is a "
+          "force type AND ratio 10^6 to 1") {
+  constexpr auto one = 1_MN;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::force_t<int64_t, std::mega>>::value);
+
+  constexpr auto one_f = 1.0_MN;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::force_t<long double, std::mega>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _GN THEN result is a "
+          "force type AND ratio 10^9 to 1") {
+  constexpr auto one = 1_GN;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::force_t<int64_t, std::giga>>::value);
+
+  constexpr auto one_f = 1.0_GN;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::force_t<long double, std::giga>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _TN THEN result is a "
+          "force type AND ratio 10^12 to 1") {
+  constexpr auto one = 1_TN;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::force_t<int64_t, std::tera>>::value);
+
+  constexpr auto one_f = 1.0_TN;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::force_t<long double, std::tera>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _PN THEN result is a "
+          "force type AND ratio 10^15 to 1") {
+  constexpr auto one = 1_PN;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::force_t<int64_t, std::peta>>::value);
+
+  constexpr auto one_f = 1.0_PN;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::force_t<long double, std::peta>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _EN THEN result is a "
+          "force type AND ratio 10^18 to 1") {
+  constexpr auto one = 1_EN;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one), const SI::force_t<int64_t, std::exa>>::value);
+
+  constexpr auto one_f = 1.0_EN;
+  STATIC_REQUIRE(std::is_same<decltype(one_f),
+                              const SI::force_t<long double, std::exa>>::value);
 }
 
 TEMPLATE_TEST_CASE("GIVEN a mass value WHEN multiplied with a acceleration "
