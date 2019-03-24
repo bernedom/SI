cb04a1708e9e1e34624c453f23adb57951cb9a6b
Dominik Berner <dominik.berner@gmail.com>
2019-03-24 21:53:45 +0100

    Electric current provides all literals for metric scaling

--- include/SI/electric_current.h
+++ include/SI/electric_current.h
@@ -8,15 +8,54 @@ template <typename _Type, typename _Ratio>
 using electric_current_t = detail::unit_t<'I', 1, _Type, _Ratio>;
 
 template <typename _Type>
-using ampere_t = electric_current_t<_Type, std::ratio<1>>;
+using atto_ampere_t = electric_current_t<_Type, std::atto>;
+template <typename _Type>
+using femto_ampere_t = electric_current_t<_Type, std::femto>;
+template <typename _Type>
+using pico_ampere_t = electric_current_t<_Type, std::pico>;
+template <typename _Type>
+using nano_ampere_t = electric_current_t<_Type, std::nano>;
+template <typename _Type>
+using micro_ampere_t = electric_current_t<_Type, std::micro>;
 template <typename _Type>
 using milli_ampere_t = electric_current_t<_Type, std::milli>;
+template <typename _Type>
+using ampere_t = electric_current_t<_Type, std::ratio<1>>;
 template <typename _Type>
 using kilo_ampere_t = electric_current_t<_Type, std::kilo>;
 template <typename _Type>
 using mega_ampere_t = electric_current_t<_Type, std::mega>;
+template <typename _Type>
+using giga_ampere_t = electric_current_t<_Type, std::giga>;
+template <typename _Type>
+using tera_ampere_t = electric_current_t<_Type, std::tera>;
+template <typename _Type>
+using peta_ampere_t = electric_current_t<_Type, std::peta>;
+template <typename _Type>
+using exa_ampere_t = electric_current_t<_Type, std::exa>;
 
 inline namespace literals {
+
+template <char... _Digits> constexpr auto operator""_aA() {
+  return SI::detail::check_overflow<atto_ampere_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_fA() {
+  return SI::detail::check_overflow<femto_ampere_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_pA() {
+  return SI::detail::check_overflow<pico_ampere_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_nA() {
+  return SI::detail::check_overflow<nano_ampere_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_uA() {
+  return SI::detail::check_overflow<micro_ampere_t<int64_t>, _Digits...>();
+}
+
 template <char... _Digits> constexpr auto operator""_mA() {
   return SI::detail::check_overflow<milli_ampere_t<int64_t>, _Digits...>();
 }
@@ -33,18 +72,72 @@ template <char... _Digits> constexpr auto operator""_MA() {
   return SI::detail::check_overflow<mega_ampere_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _mA(long double mA) {
-  return milli_ampere_t<long double>(mA);
+template <char... _Digits> constexpr auto operator""_GA() {
+  return SI::detail::check_overflow<giga_ampere_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _A(long double A) { return ampere_t<long double>(A); }
-
-constexpr auto operator"" _kA(long double kA) {
-  return kilo_ampere_t<long double>(kA);
+template <char... _Digits> constexpr auto operator""_TA() {
+  return SI::detail::check_overflow<tera_ampere_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _MA(long double MA) {
-  return mega_ampere_t<long double>(MA);
+template <char... _Digits> constexpr auto operator""_PA() {
+  return SI::detail::check_overflow<peta_ampere_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_EA() {
+  return SI::detail::check_overflow<exa_ampere_t<int64_t>, _Digits...>();
+}
+
+constexpr auto operator""_aA(long double value) {
+  return atto_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_fA(long double value) {
+  return femto_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_pA(long double value) {
+  return pico_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_nA(long double value) {
+  return nano_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_uA(long double value) {
+  return micro_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_mA(long double value) {
+  return milli_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_A(long double value) {
+  return ampere_t<long double>{value};
+}
+
+constexpr auto operator""_kA(long double value) {
+  return kilo_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_MA(long double value) {
+  return mega_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_GA(long double value) {
+  return giga_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_TA(long double value) {
+  return tera_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_PA(long double value) {
+  return peta_ampere_t<long double>{value};
+}
+
+constexpr auto operator""_EA(long double value) {
+  return exa_ampere_t<long double>{value};
 }
 
 } // namespace literals
--- test/src/base_unit_tests/electric_current_tests.cc
+++ test/src/base_unit_tests/electric_current_tests.cc
@@ -4,17 +4,69 @@
 
 using namespace SI::literals;
 
-TEST_CASE("GIVEN a value WHEN constructed with literal _A THEN result is a "
-          "electric_current type AND ratio 1 to 1") {
-  constexpr auto one = 1_A;
-  STATIC_REQUIRE(std::is_same<
-                 decltype(one),
-                 const SI::electric_current_t<int64_t, std::ratio<1>>>::value);
-
-  constexpr auto one_f = 1.0_A;
+TEST_CASE("GIVEN a value WHEN constructed with literal _pA THEN result is a "
+          "electric_current type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_aA;
   STATIC_REQUIRE(
-      std::is_same<decltype(one_f), const SI::electric_current_t<
-                                        long double, std::ratio<1>>>::value);
+      std::is_same<decltype(one),
+                   const SI::electric_current_t<int64_t, std::atto>>::value);
+
+  constexpr auto one_f = 1.0_aA;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(one_f),
+                 const SI::electric_current_t<long double, std::atto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _pA THEN result is a "
+          "electric_current type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_fA;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::electric_current_t<int64_t, std::femto>>::value);
+
+  constexpr auto one_f = 1.0_fA;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(one_f),
+                 const SI::electric_current_t<long double, std::femto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _pA THEN result is a "
+          "electric_current type AND ratio 1 to 10^12") {
+  constexpr auto one = 1_pA;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::electric_current_t<int64_t, std::pico>>::value);
+
+  constexpr auto one_f = 1.0_pA;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(one_f),
+                 const SI::electric_current_t<long double, std::pico>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _nA THEN result is a "
+          "electric_current type AND ratio 1 to 10^9") {
+  constexpr auto one = 1_nA;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::electric_current_t<int64_t, std::nano>>::value);
+
+  constexpr auto one_f = 1.0_nA;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(one_f),
+                 const SI::electric_current_t<long double, std::nano>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _uA THEN result is a "
+          "electric_current type AND ratio 1 to 10^6") {
+  constexpr auto one = 1_uA;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::electric_current_t<int64_t, std::micro>>::value);
+
+  constexpr auto one_f = 1.0_uA;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(one_f),
+                 const SI::electric_current_t<long double, std::micro>>::value);
 }
 
 TEST_CASE("GIVEN a value WHEN constructed with literal _mA THEN result is a "
@@ -28,6 +80,19 @@ TEST_CASE("GIVEN a value WHEN constructed with literal _mA THEN result is a "
   STATIC_REQUIRE(std::is_same<
                  decltype(one_f),
                  const SI::electric_current_t<long double, std::milli>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _A THEN result is a "
+          "electric_current type AND ratio 1 to 1") {
+  constexpr auto one = 1_A;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(one),
+                 const SI::electric_current_t<int64_t, std::ratio<1>>>::value);
+
+  constexpr auto one_f = 1.0_A;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f), const SI::electric_current_t<
+                                        long double, std::ratio<1>>>::value);
 }
 
 TEST_CASE("GIVEN a value WHEN constructed with literal _kA THEN result is a "
@@ -44,7 +109,7 @@ TEST_CASE("GIVEN a value WHEN constructed with literal _kA THEN result is a "
 }
 
 TEST_CASE("GIVEN a value WHEN constructed with literal _MA THEN result is a "
-          "electric_current type AND ratio 1000000 to 1") {
+          "electric_current type AND ratio 10^6 to 1") {
   constexpr auto one = 1_MA;
   STATIC_REQUIRE(
       std::is_same<decltype(one),
@@ -54,4 +119,56 @@ TEST_CASE("GIVEN a value WHEN constructed with literal _MA THEN result is a "
   STATIC_REQUIRE(std::is_same<
                  decltype(one_f),
                  const SI::electric_current_t<long double, std::mega>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _GA THEN result is a "
+          "electric_current type AND ratio 10^9 to 1") {
+  constexpr auto one = 1_GA;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::electric_current_t<int64_t, std::giga>>::value);
+
+  constexpr auto one_f = 1.0_GA;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(one_f),
+                 const SI::electric_current_t<long double, std::giga>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _TA THEN result is a "
+          "electric_current type AND ratio 10^12 to 1") {
+  constexpr auto one = 1_TA;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::electric_current_t<int64_t, std::tera>>::value);
+
+  constexpr auto one_f = 1.0_TA;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(one_f),
+                 const SI::electric_current_t<long double, std::tera>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _PA THEN result is a "
+          "electric_current type AND ratio 10^15 to 1") {
+  constexpr auto one = 1_PA;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::electric_current_t<int64_t, std::peta>>::value);
+
+  constexpr auto one_f = 1.0_PA;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(one_f),
+                 const SI::electric_current_t<long double, std::peta>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _EA THEN result is a "
+          "electric_current type AND ratio 10^18 to 1") {
+  constexpr auto one = 1_EA;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one),
+                   const SI::electric_current_t<int64_t, std::exa>>::value);
+
+  constexpr auto one_f = 1.0_EA;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::electric_current_t<long double, std::exa>>::value);
 }
