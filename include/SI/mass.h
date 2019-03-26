b3384e37144a111f8419ca495c563bef94d17b75
Dominik Berner <dominik.berner@gmail.com>
2019-03-26 12:32:30 +0100

    Metric prefixes for mass from femto-gram to ton

--- include/SI/mass.h
+++ include/SI/mass.h
@@ -6,33 +6,75 @@ namespace SI {
 template <typename _Type, typename _Ratio>
 using mass_t = detail::unit_t<'M', 1, _Type, _Ratio>;
 
-template <typename _Type> using gram_t = mass_t<_Type, std::milli>;
-
-template <typename _Type> using kilo_gram_t = mass_t<_Type, std::ratio<1>>;
+template <typename _Type> using femto_gram_t = mass_t<_Type, std::atto>;
+template <typename _Type> using pico_gram_t = mass_t<_Type, std::femto>;
+template <typename _Type> using nano_gram_t = mass_t<_Type, std::pico>;
+template <typename _Type> using micro_gram_t = mass_t<_Type, std::nano>;
 template <typename _Type> using milli_gram_t = mass_t<_Type, std::micro>;
+template <typename _Type> using gram_t = mass_t<_Type, std::milli>;
+template <typename _Type> using kilo_gram_t = mass_t<_Type, std::ratio<1>>;
+template <typename _Type> using ton_t = mass_t<_Type, std::kilo>;
 
 inline namespace literals {
-template <char... _Digits> constexpr auto operator""_g() {
-  return SI::detail::check_overflow<gram_t<int64_t>, _Digits...>();
+
+template <char... _Digits> constexpr auto operator""_fg() {
+  return SI::detail::check_overflow<femto_gram_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_pg() {
+  return SI::detail::check_overflow<pico_gram_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_ng() {
+  return SI::detail::check_overflow<nano_gram_t<int64_t>, _Digits...>();
+}
+
+template <char... _Digits> constexpr auto operator""_ug() {
+  return SI::detail::check_overflow<micro_gram_t<int64_t>, _Digits...>();
 }
 
 template <char... _Digits> constexpr auto operator""_mg() {
   return SI::detail::check_overflow<milli_gram_t<int64_t>, _Digits...>();
 }
 
+template <char... _Digits> constexpr auto operator""_g() {
+  return SI::detail::check_overflow<gram_t<int64_t>, _Digits...>();
+}
+
 template <char... _Digits> constexpr auto operator""_kg() {
   return SI::detail::check_overflow<kilo_gram_t<int64_t>, _Digits...>();
 }
 
-constexpr auto operator"" _g(long double g) { return gram_t<long double>(g); }
+template <char... _Digits> constexpr auto operator""_t() {
+  return SI::detail::check_overflow<ton_t<int64_t>, _Digits...>();
+}
+
+constexpr auto operator""_fg(long double value) {
+  return femto_gram_t<long double>{value};
+}
+
+constexpr auto operator""_pg(long double value) {
+  return pico_gram_t<long double>{value};
+}
+
+constexpr auto operator""_ng(long double value) {
+  return nano_gram_t<long double>{value};
+}
+
+constexpr auto operator""_ug(long double value) {
+  return micro_gram_t<long double>{value};
+}
 
 constexpr auto operator"" _mg(long double mg) {
   return milli_gram_t<long double>(mg);
 }
 
+constexpr auto operator"" _g(long double g) { return gram_t<long double>(g); }
+
 constexpr auto operator"" _kg(long double kg) {
   return kilo_gram_t<long double>(kg);
 }
+constexpr auto operator"" _t(long double t) { return ton_t<long double>(t); }
 
 } // namespace literals
 } // namespace SI
--- test/src/base_unit_tests/mass_tests.cc
+++ test/src/base_unit_tests/mass_tests.cc
@@ -4,16 +4,49 @@
 
 using namespace SI::literals;
 
-TEST_CASE("GIVEN a value WHEN constructed with literal _g THEN result is a "
-          "mass value AND ratio is 1/1000") {
-  constexpr auto one = 1_g;
-  STATIC_REQUIRE(std::is_same<decltype(one),
-                              const SI::mass_t<int64_t, std::milli>>::value);
+TEST_CASE("GIVEN a value WHEN constructed with literal _fg THEN result is a "
+          "mass type AND ratio 1 to 10^18") {
+  constexpr auto one = 1_fg;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one), const SI::mass_t<int64_t, std::atto>>::value);
 
-  constexpr auto one_f = 1.0_g;
+  constexpr auto one_f = 1.0_fg;
+  STATIC_REQUIRE(std::is_same<decltype(one_f),
+                              const SI::mass_t<long double, std::atto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _pg THEN result is a "
+          "mass type AND ratio 1 to 10^15") {
+  constexpr auto one = 1_pg;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::mass_t<int64_t, std::femto>>::value);
+
+  constexpr auto one_f = 1.0_pg;
   STATIC_REQUIRE(
       std::is_same<decltype(one_f),
-                   const SI::mass_t<long double, std::milli>>::value);
+                   const SI::mass_t<long double, std::femto>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _ng THEN result is a "
+          "mass type AND ratio 1 to 10^12") {
+  constexpr auto one = 1_ng;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one), const SI::mass_t<int64_t, std::pico>>::value);
+
+  constexpr auto one_f = 1.0_ng;
+  STATIC_REQUIRE(std::is_same<decltype(one_f),
+                              const SI::mass_t<long double, std::pico>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _ug THEN result is a "
+          "mass type AND ratio 1 to 10^9") {
+  constexpr auto one = 1_ug;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one), const SI::mass_t<int64_t, std::nano>>::value);
+
+  constexpr auto one_f = 1.0_ug;
+  STATIC_REQUIRE(std::is_same<decltype(one_f),
+                              const SI::mass_t<long double, std::nano>>::value);
 }
 
 TEST_CASE("GIVEN a value WHEN constructed with literal _mg THEN result is a "
@@ -29,6 +62,18 @@ TEST_CASE("GIVEN a value WHEN constructed with literal _mg THEN result is a "
                    const SI::mass_t<long double, std::micro>>::value);
 }
 
+TEST_CASE("GIVEN a value WHEN constructed with literal _g THEN result is a "
+          "mass value AND ratio is 1/1000") {
+  constexpr auto one = 1_g;
+  STATIC_REQUIRE(std::is_same<decltype(one),
+                              const SI::mass_t<int64_t, std::milli>>::value);
+
+  constexpr auto one_f = 1.0_g;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one_f),
+                   const SI::mass_t<long double, std::milli>>::value);
+}
+
 TEST_CASE("GIVEN a value WHEN constructed with literal _kg THEN result is a "
           "mass value AND ratio is 1/1") {
   constexpr auto one = 1_kg;
@@ -39,4 +84,15 @@ TEST_CASE("GIVEN a value WHEN constructed with literal _kg THEN result is a "
   STATIC_REQUIRE(
       std::is_same<decltype(one_f),
                    const SI::mass_t<long double, std::ratio<1>>>::value);
+}
+
+TEST_CASE("GIVEN a value WHEN constructed with literal _t THEN result is a "
+          "mass value AND ratio is 1000/1") {
+  constexpr auto one = 1_t;
+  STATIC_REQUIRE(
+      std::is_same<decltype(one), const SI::mass_t<int64_t, std::kilo>>::value);
+
+  constexpr auto one_f = 1.0_t;
+  STATIC_REQUIRE(std::is_same<decltype(one_f),
+                              const SI::mass_t<long double, std::kilo>>::value);
 }
