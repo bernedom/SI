f3c9e39546f184eb59571d66f0fdb4b425f06b2a
Dominik Berner <dominik.berner@gmail.com>
2019-07-15 22:25:18 +0200

    Added skeleton for compilation tests

--- test/compilation-tests.sh
+++ test/compilation-tests.sh
@@ -3,37 +3,66 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR=$(realpath ${SCRIPT_DIR}/../)
+INSTALL_PATH=$(realpath ~/SI-install)
 
-testSuccessfulCompilationWhenDefaultInvocation() {
-  TMP_DIR=$(mktemp -d)
-
-  cd ${TMP_DIR}
-  cmake ${ROOT_DIR} -G Ninja
-  assertEquals "Configuration successful" $? 0
-  cmake --build . --config Release 
-  assertEquals "Building successful" $? 0
-  
-  if [ -d ${TMP_DIR} ]; then
-    rm -rf ${TMP_DIR}
-  fi
-  cd ${ROOT_DIR}
+oneTimeSetUp(){
+    
+    TMP_DIR=$(mktemp -d)
+    
+    # install SI
+    cmake ${ROOT_DIR} -B${TMP_DIR} -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PATH} -DBUILD_TESTING=off -G Ninja
+    cmake --build ${TMP_DIR} --config Release --target install
+    
+    if [ -d ${TMP_DIR} ]; then
+        rm -rf ${TMP_DIR}
+    fi
 }
 
-testFailedCompilationWhenImplicitConversionDisabled() {
-  TMP_DIR=$(mktemp -d)
-
-  cd ${TMP_DIR}
-  cmake ${ROOT_DIR} -DCMAKE_CXX_FLAGS="-DENABLE_IMPLICIT_RATIO_CONVERSION=false" -G Ninja
-  assertEquals "Configuration successful" $? 0
-  cmake --build . --config Release 
-  assertNotEquals "Building fails" $? 0
-  
-  if [ -d ${TMP_DIR} ]; then
-    rm -rf ${TMP_DIR}
-  fi
-  cd ${ROOT_DIR}
+setUp(){
+    TMP_DIR=$(mktemp -d)
 }
 
+tearDown(){
+    if [ -d ${TMP_DIR} ]; then
+        rm -rf ${TMP_DIR}
+    fi
+}
+
+testSISelfSuccessfulCompilationWhenDefaultInvocation() {
+    
+    cmake ${ROOT_DIR} -B${TMP_DIR} -G Ninja
+    assertEquals "Configuration successful" $? 0
+    cmake --build ${TMP_DIR} --config Release
+    assertEquals "Building successful" $? 0
+    
+    
+}
+
+testSISelfFailedCompilationWhenImplicitConversionDisabled() {
+    
+    cmake ${ROOT_DIR} -B${TMP_DIR} -DCMAKE_CXX_FLAGS="-DENABLE_IMPLICIT_RATIO_CONVERSION=false" -G Ninja
+    assertEquals "Configuration successful" $? 0
+    cmake --build ${TMP_DIR} --config Release
+    assertNotEquals "Building fails" $? 0
+    
+    
+}
+
+testOperatorEqualsCompilesWhenDefaultInvocation()
+{
+    cmake ${ROOT_DIR}/test/src/compilation_tests/ -B${TMP_DIR} -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PATH} -G Ninja
+    assertEquals "Configuration successful" $? 0
+    cmake --build ${TMP_DIR} --config Release
+}
+
+
+testOperatorEqualsFailsWhenImplicitConversionDisabled()
+{
+    cmake ${ROOT_DIR}/test/src/compilation_tests/ -B${TMP_DIR} -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PATH} -DCMAKE_CXX_FLAGS="-DENABLE_IMPLICIT_RATIO_CONVERSION=false" -G Ninja
+    assertEquals "Configuration successful" $? 0
+    cmake --build ${TMP_DIR} --config Release
+    assertNotEquals "Building fails" $? 0
+}
 
 # Load shUnit2.
 . shunit2
---
+++ test/src/compilation_tests/operator_equals.cc
@@ -0,0 +1,8 @@
+#include <SI/detail/unit.h>
+
+int func() {
+  constexpr SI::detail::unit_t<'X', 1, int64_t, std::ratio<1>> v1{1};
+  constexpr SI::detail::unit_t<'X', 1, int64_t, std::kilo> v2{1};
+
+  constexpr auto r = v1 == v2;
+}
