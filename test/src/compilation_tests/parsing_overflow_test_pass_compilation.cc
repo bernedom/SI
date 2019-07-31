3ac5664bcef1d8992cf9ff1cb323166b500777c4
Dominik Berner <dominik.berner@gmail.com>
2019-07-31 10:44:25 +0200

    Added compilation test for numberparser overflow

---
+++ test/src/compilation_tests/parsing_overflow_test_pass_compilation.cc
@@ -0,0 +1,18 @@
+#include <SI/detail/number_parser.h>
+
+/// helper literal
+
+namespace {
+template <char... _Digits> constexpr int64_t operator""_literal_op() {
+  return SI::detail::parsing::Number<_Digits...>::value;
+}
+
+int overflow_test_pass() {
+  constexpr auto v = 0xFFFFFFFFFFFFFFF_literal_op;
+  /// needed to avoid unused variable warning
+  if (v > 0) {
+    return 1;
+  }
+  return 0;
+}
+} // namespace
