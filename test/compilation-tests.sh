388d8210c6e1ac26c50ec42ff76218c3993b0d37
Dominik Berner <dominik.berner@gmail.com>
2019-08-03 22:48:48 +0200

    Cleanup and typos

--- test/compilation-tests.sh
+++ test/compilation-tests.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # file: test/compilation-tests.sh
 
-# TODO add tests for generated operators
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR=$(realpath ${SCRIPT_DIR}/../)
 INSTALL_PATH=$(realpath ~/SI-install)
