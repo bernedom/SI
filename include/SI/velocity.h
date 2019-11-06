0a4793817b5ea6de9df1b96d23cce022b9799684
Dominik Berner <dominik.berner@gmail.com>
2019-11-06 07:34:41 +0100

    Added version number to header files

--- include/SI/velocity.h
+++ include/SI/velocity.h
@@ -1,5 +1,5 @@
 /**
- * This file is part of "SI",
+ * This file is part of "SI" version 1.3.1
  * A header only c++ library that provides type safety and user defined literals
  * for handling pyhsical values defined in the International System of
  * Units
