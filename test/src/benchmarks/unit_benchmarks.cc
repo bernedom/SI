60e6d0f966c99aac6c7bcb8a39cf462c5561822b
Dominik Berner <dominik.berner@gmail.com>
2019-12-06 22:56:10 +0100

    Added first micro-benchmark

--- test/src/benchmarks/unit_benchmarks.cc
+++ test/src/benchmarks/unit_benchmarks.cc
@@ -1,1 +1,18 @@
 #include <catch2/catch.hpp>
+
+#include <SI/detail/unit.h>
+
+using namespace SI::detail;
+
+TEST_CASE("Unit_assignment") {
+  BENCHMARK("Same Unit assignment") {
+    constexpr unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
+    unit_t<'X', 1, int64_t, std::ratio<1>> v2{0};
+    v2 = v1;
+  };
+  BENCHMARK("Same Unit different ratio assignment") {
+    constexpr unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
+    unit_t<'X', 1, int64_t, std::milli> v2{2000};
+    v2 = v1;
+  };
+}
