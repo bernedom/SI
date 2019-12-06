89a1e7409a02bc59b69bc2fe471c8fbc1b253abe
Dominik Berner <dominik.berner@gmail.com>
2019-12-06 23:09:43 +0100

    Advanced benchmarks

--- test/src/benchmarks/unit_benchmarks.cc
+++ test/src/benchmarks/unit_benchmarks.cc
@@ -6,13 +6,25 @@ using namespace SI::detail;
 
 TEST_CASE("Unit_assignment") {
   BENCHMARK("Same Unit assignment") {
-    constexpr unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
+    unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
     unit_t<'X', 1, int64_t, std::ratio<1>> v2{0};
     v2 = v1;
   };
   BENCHMARK("Same Unit different ratio assignment") {
-    constexpr unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
+    unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
     unit_t<'X', 1, int64_t, std::milli> v2{2000};
     v2 = v1;
+  };
+  BENCHMARK_ADVANCED("Same Unit assignment adv")
+  (Catch::Benchmark::Chronometer meter) {
+    unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
+    unit_t<'X', 1, int64_t, std::ratio<1>> v2{0};
+    meter.measure([&] { v2 = v1; });
+  };
+  BENCHMARK_ADVANCED("Same Unit different ratio assignment adv")
+  (Catch::Benchmark::Chronometer meter) {
+    unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
+    unit_t<'X', 1, int64_t, std::milli> v2{1000};
+    meter.measure([&] { v2 = v1; });
   };
 }
