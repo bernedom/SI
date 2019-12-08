aa412957a5183d0fedfa0411e6151c292c56037e
Dominik Berner <dominik.berner@gmail.com>
2019-12-08 22:01:25 +0100

    More computation for benchmark

--- test/src/benchmarks/unit_benchmarks.cc
+++ test/src/benchmarks/unit_benchmarks.cc
@@ -4,27 +4,42 @@
 
 using namespace SI::detail;
 
+namespace {
+using ratio_one_unit = unit_t<'X', 1, int64_t, std::ratio<1>>;
+using ratio_kilo_unit = unit_t<'X', 1, int64_t, std::kilo>;
+using ratio_milli_unit = unit_t<'X', 1, int64_t, std::milli>;
+} // namespace
+
 TEST_CASE("Unit_assignment") {
-  BENCHMARK("Same Unit assignment") {
-    unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
-    unit_t<'X', 1, int64_t, std::ratio<1>> v2{0};
-    v2 = v1;
-  };
-  BENCHMARK("Same Unit different ratio assignment") {
-    unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
-    unit_t<'X', 1, int64_t, std::milli> v2{2000};
-    v2 = v1;
-  };
-  BENCHMARK_ADVANCED("Same Unit assignment adv")
+
+  BENCHMARK_ADVANCED("Same unit same ratio assignment")
   (Catch::Benchmark::Chronometer meter) {
-    unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
-    unit_t<'X', 1, int64_t, std::ratio<1>> v2{0};
-    meter.measure([&] { v2 = v1; });
+    std::vector<ratio_one_unit> source(10000);
+    std::vector<ratio_one_unit> target(10000, 0);
+    for (size_t i = 0; i < source.size(); ++i) {
+      source[i] = i;
+    }
+
+    meter.measure([&] {
+      for (size_t i = 0; i < source.size(); ++i) {
+        target[i] = source[i];
+      }
+      return target;
+    });
   };
-  BENCHMARK_ADVANCED("Same Unit different ratio assignment adv")
+  BENCHMARK_ADVANCED("Same unit different ratio assignment")
   (Catch::Benchmark::Chronometer meter) {
-    unit_t<'X', 1, int64_t, std::ratio<1>> v1{123};
-    unit_t<'X', 1, int64_t, std::milli> v2{1000};
-    meter.measure([&] { v2 = v1; });
+    std::vector<ratio_one_unit> source(10000);
+    std::vector<ratio_milli_unit> target(10000, 0);
+    for (size_t i = 0; i < source.size(); ++i) {
+      source[i] = i;
+    }
+
+    meter.measure([&] {
+      for (size_t i = 0; i < source.size(); ++i) {
+        target[i] = source[i];
+      }
+      return target;
+    });
   };
 }
