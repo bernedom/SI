40986534a51e29eef530e236ae062f159606739b
github-action-benchmark <github@users.noreply.github.com>
2020-01-21 21:48:39 +0000

    add Catch2 Benchmark (catch2) benchmark result for 6e85cdf5c2b7369fc80d6da5c6819ab30d6e1490

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1579643319540,
+  "repoUrl": "https://github.com/bernedom/SI",
+  "entries": {
+    "Catch2 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "dominik.berner@gmail.com",
+            "name": "Dominik Berner",
+            "username": "bernedom"
+          },
+          "committer": {
+            "email": "dominik.berner@gmail.com",
+            "name": "Dominik Berner",
+            "username": "bernedom"
+          },
+          "distinct": true,
+          "id": "6e85cdf5c2b7369fc80d6da5c6819ab30d6e1490",
+          "message": "Changed workflow name",
+          "timestamp": "2020-01-21T22:47:27+01:00",
+          "tree_id": "b112795c1516b3f01c97c62e7a0f6b27cce87499",
+          "url": "https://github.com/bernedom/SI/commit/6e85cdf5c2b7369fc80d6da5c6819ab30d6e1490"
+        },
+        "date": 1579643319165,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Same unit same ratio assignment",
+            "value": 8.437,
+            "range": "+/- 2.529",
+            "unit": "us",
+            "extra": "100 samples\n5 iterations"
+          },
+          {
+            "name": "Same unit different ratio assignment",
+            "value": 9.674,
+            "range": "+/- 2.695",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
