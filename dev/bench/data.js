67bbe59fd46084af6cc0e84afe649e8db1bad42a
github-action-benchmark <github@users.noreply.github.com>
2020-01-21 22:01:33 +0000

    add Catch2 Benchmark (catch2) benchmark result for 78148bed800dee60fcac0e3dd3433950ddd9c47e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,126 +1,175 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1579643821949,
-  "repoUrl": "https://github.com/bernedom/SI",
-  "entries": {
-    "Catch2 Benchmark": [
+  'lastUpdate': 1579644093528,
+  'repoUrl': 'https://github.com/bernedom/SI',
+  'entries': {
+    'Catch2 Benchmark': [
       {
-        "commit": {
-          "author": {
-            "email": "dominik.berner@gmail.com",
-            "name": "Dominik Berner",
-            "username": "bernedom"
+        'commit': {
+          'author': {
+            'email': 'dominik.berner@gmail.com',
+            'name': 'Dominik Berner',
+            'username': 'bernedom'
           },
-          "committer": {
-            "email": "dominik.berner@gmail.com",
-            "name": "Dominik Berner",
-            "username": "bernedom"
+          'committer': {
+            'email': 'dominik.berner@gmail.com',
+            'name': 'Dominik Berner',
+            'username': 'bernedom'
           },
-          "distinct": true,
-          "id": "6e85cdf5c2b7369fc80d6da5c6819ab30d6e1490",
-          "message": "Changed workflow name",
-          "timestamp": "2020-01-21T22:47:27+01:00",
-          "tree_id": "b112795c1516b3f01c97c62e7a0f6b27cce87499",
-          "url": "https://github.com/bernedom/SI/commit/6e85cdf5c2b7369fc80d6da5c6819ab30d6e1490"
+          'distinct': true,
+          'id': '6e85cdf5c2b7369fc80d6da5c6819ab30d6e1490',
+          'message': 'Changed workflow name',
+          'timestamp': '2020-01-21T22:47:27+01:00',
+          'tree_id': 'b112795c1516b3f01c97c62e7a0f6b27cce87499',
+          'url':
+              'https://github.com/bernedom/SI/commit/6e85cdf5c2b7369fc80d6da5c6819ab30d6e1490'
         },
-        "date": 1579643319165,
-        "tool": "catch2",
-        "benches": [
+        'date': 1579643319165,
+        'tool': 'catch2',
+        'benches': [
           {
-            "name": "Same unit same ratio assignment",
-            "value": 8.437,
-            "range": "+/- 2.529",
-            "unit": "us",
-            "extra": "100 samples\n5 iterations"
+            'name': 'Same unit same ratio assignment',
+            'value': 8.437,
+            'range': '+/- 2.529',
+            'unit': 'us',
+            'extra': '100 samples\n5 iterations'
           },
           {
-            "name": "Same unit different ratio assignment",
-            "value": 9.674,
-            "range": "+/- 2.695",
-            "unit": "us",
-            "extra": "100 samples\n4 iterations"
+            'name': 'Same unit different ratio assignment',
+            'value': 9.674,
+            'range': '+/- 2.695',
+            'unit': 'us',
+            'extra': '100 samples\n4 iterations'
           }
         ]
       },
       {
-        "commit": {
-          "author": {
-            "email": "dominik.berner@gmail.com",
-            "name": "Dominik Berner",
-            "username": "bernedom"
+        'commit': {
+          'author': {
+            'email': 'dominik.berner@gmail.com',
+            'name': 'Dominik Berner',
+            'username': 'bernedom'
           },
-          "committer": {
-            "email": "dominik.berner@gmail.com",
-            "name": "Dominik Berner",
-            "username": "bernedom"
+          'committer': {
+            'email': 'dominik.berner@gmail.com',
+            'name': 'Dominik Berner',
+            'username': 'bernedom'
           },
-          "distinct": true,
-          "id": "fd94f3859ac470bc244281e70ab50734f93e3b6b",
-          "message": "Installing conan",
-          "timestamp": "2020-01-21T22:50:10+01:00",
-          "tree_id": "b112795c1516b3f01c97c62e7a0f6b27cce87499",
-          "url": "https://github.com/bernedom/SI/commit/fd94f3859ac470bc244281e70ab50734f93e3b6b"
+          'distinct': true,
+          'id': 'fd94f3859ac470bc244281e70ab50734f93e3b6b',
+          'message': 'Installing conan',
+          'timestamp': '2020-01-21T22:50:10+01:00',
+          'tree_id': 'b112795c1516b3f01c97c62e7a0f6b27cce87499',
+          'url':
+              'https://github.com/bernedom/SI/commit/fd94f3859ac470bc244281e70ab50734f93e3b6b'
         },
-        "date": 1579643482736,
-        "tool": "catch2",
-        "benches": [
+        'date': 1579643482736,
+        'tool': 'catch2',
+        'benches': [
           {
-            "name": "Same unit same ratio assignment",
-            "value": 9.182,
-            "range": "+/- 3.843",
-            "unit": "us",
-            "extra": "100 samples\n4 iterations"
+            'name': 'Same unit same ratio assignment',
+            'value': 9.182,
+            'range': '+/- 3.843',
+            'unit': 'us',
+            'extra': '100 samples\n4 iterations'
           },
           {
-            "name": "Same unit different ratio assignment",
-            "value": 10.89,
-            "range": "+/- 1.981",
-            "unit": "us",
-            "extra": "100 samples\n4 iterations"
+            'name': 'Same unit different ratio assignment',
+            'value': 10.89,
+            'range': '+/- 1.981',
+            'unit': 'us',
+            'extra': '100 samples\n4 iterations'
           }
         ]
       },
       {
-        "commit": {
-          "author": {
-            "email": "dominik.berner@gmail.com",
-            "name": "Dominik Berner",
-            "username": "bernedom"
+        'commit': {
+          'author': {
+            'email': 'dominik.berner@gmail.com',
+            'name': 'Dominik Berner',
+            'username': 'bernedom'
           },
-          "committer": {
-            "email": "dominik.berner@gmail.com",
-            "name": "Dominik Berner",
-            "username": "bernedom"
+          'committer': {
+            'email': 'dominik.berner@gmail.com',
+            'name': 'Dominik Berner',
+            'username': 'bernedom'
           },
-          "distinct": true,
-          "id": "20376504a69e870f9a8fd2faba7723827e210c05",
-          "message": "Added reference benchmark",
-          "timestamp": "2020-01-21T22:55:50+01:00",
-          "tree_id": "fea0db17abdcb4c59db9e4bc8a2e443edda5c9c6",
-          "url": "https://github.com/bernedom/SI/commit/20376504a69e870f9a8fd2faba7723827e210c05"
+          'distinct': true,
+          'id': '20376504a69e870f9a8fd2faba7723827e210c05',
+          'message': 'Added reference benchmark',
+          'timestamp': '2020-01-21T22:55:50+01:00',
+          'tree_id': 'fea0db17abdcb4c59db9e4bc8a2e443edda5c9c6',
+          'url':
+              'https://github.com/bernedom/SI/commit/20376504a69e870f9a8fd2faba7723827e210c05'
         },
-        "date": 1579643821540,
-        "tool": "catch2",
-        "benches": [
+        'date': 1579643821540,
+        'tool': 'catch2',
+        'benches': [
           {
-            "name": "Raw number assignment (reference)",
-            "value": 9.129,
-            "range": "+/- 482",
-            "unit": "us",
-            "extra": "100 samples\n4 iterations"
+            'name': 'Raw number assignment (reference)',
+            'value': 9.129,
+            'range': '+/- 482',
+            'unit': 'us',
+            'extra': '100 samples\n4 iterations'
           },
           {
-            "name": "Same unit same ratio assignment",
-            "value": 8.387,
-            "range": "+/- 2.126",
-            "unit": "us",
-            "extra": "100 samples\n5 iterations"
+            'name': 'Same unit same ratio assignment',
+            'value': 8.387,
+            'range': '+/- 2.126',
+            'unit': 'us',
+            'extra': '100 samples\n5 iterations'
           },
           {
-            "name": "Same unit different ratio assignment",
-            "value": 9.951,
-            "range": "+/- 329",
-            "unit": "us",
-            "extra": "100 samples\n3 iterations"
+            'name': 'Same unit different ratio assignment',
+            'value': 9.951,
+            'range': '+/- 329',
+            'unit': 'us',
+            'extra': '100 samples\n3 iterations'
+          }
+        ]
+      },
+      {
+        'commit': {
+          'author': {
+            'email': 'dominik.berner@gmail.com',
+            'name': 'Dominik Berner',
+            'username': 'bernedom'
+          },
+          'committer': {
+            'email': 'dominik.berner@gmail.com',
+            'name': 'Dominik Berner',
+            'username': 'bernedom'
+          },
+          'distinct': true,
+          'id': '78148bed800dee60fcac0e3dd3433950ddd9c47e',
+          'message': 'Added reference benchmark',
+          'timestamp': '2020-01-21T23:00:20+01:00',
+          'tree_id': '578c74fc1343528db505c0c1b23c9961da972264',
+          'url':
+              'https://github.com/bernedom/SI/commit/78148bed800dee60fcac0e3dd3433950ddd9c47e'
+        },
+        'date': 1579644093005,
+        'tool': 'catch2',
+        'benches': [
+          {
+            'name': 'Raw number assignment (reference)',
+            'value': 7.743,
+            'range': '+/- 804',
+            'unit': 'us',
+            'extra': '100 samples\n4 iterations'
+          },
+          {
+            'name': 'Same unit same ratio assignment',
+            'value': 8.454,
+            'range': '+/- 2.396',
+            'unit': 'us',
+            'extra': '100 samples\n5 iterations'
+          },
+          {
+            'name': 'Same unit different ratio assignment',
+            'value': 9.882,
+            'range': '+/- 3.895',
+            'unit': 'us',
+            'extra': '100 samples\n3 iterations'
           }
         ]
       }
