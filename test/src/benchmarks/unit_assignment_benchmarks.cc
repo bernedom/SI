4f035078cee786d6421d35108b870b53c9dad064
Dominik Berner <dominik.berner@gmail.com>
2020-01-23 21:46:05 +0100

    Added add and subtract assignment

--- test/src/benchmarks/unit_assignment_benchmarks.cc
+++ test/src/benchmarks/unit_assignment_benchmarks.cc
@@ -86,4 +86,148 @@ TEST_CASE("Unit_assignment") {
       });
     };
   }
+
+  SECTION("Add-Assignment") {
+    BENCHMARK_ADVANCED("Raw number add assignment (reference)")
+    (Catch::Benchmark::Chronometer meter) {
+      std::vector<int64_t> source(10000);
+      std::vector<int64_t> target(10000, 0);
+      for (size_t i = 0; i < source.size(); ++i) {
+        source[i] = i;
+      }
+
+      meter.measure([&] {
+        for (size_t i = 0; i < source.size(); ++i) {
+          target[i] += source[i];
+        }
+        return target;
+      });
+    };
+    BENCHMARK_ADVANCED("Same unit same ratio add assignment")
+    (Catch::Benchmark::Chronometer meter) {
+      std::vector<ratio_one_unit> source(10000);
+      std::vector<ratio_one_unit> target(10000, 0);
+      for (size_t i = 0; i < source.size(); ++i) {
+        source[i] = i;
+      }
+
+      meter.measure([&] {
+        for (size_t i = 0; i < source.size(); ++i) {
+          target[i] += source[i];
+        }
+        return target;
+      });
+    };
+    BENCHMARK_ADVANCED("Same unit different ratio add assignment")
+    (Catch::Benchmark::Chronometer meter) {
+      std::vector<ratio_one_unit> source(10000);
+      std::vector<ratio_milli_unit> target(10000, 0);
+      for (size_t i = 0; i < source.size(); ++i) {
+        source[i] = i;
+      }
+
+      meter.measure([&] {
+        for (size_t i = 0; i < source.size(); ++i) {
+          target[i] += source[i];
+        }
+        return target;
+      });
+    };
+  }
+
+  SECTION("Subtract-Assignment") {
+    BENCHMARK_ADVANCED("Raw number subtract assignment (reference)")
+    (Catch::Benchmark::Chronometer meter) {
+      std::vector<int64_t> source(10000);
+      std::vector<int64_t> target(10000, 0);
+      for (size_t i = 0; i < source.size(); ++i) {
+        source[i] = i;
+      }
+
+      meter.measure([&] {
+        for (size_t i = 0; i < source.size(); ++i) {
+          target[i] -= source[i];
+        }
+        return target;
+      });
+    };
+    BENCHMARK_ADVANCED("Same unit same ratio subtract assignment")
+    (Catch::Benchmark::Chronometer meter) {
+      std::vector<ratio_one_unit> source(10000);
+      std::vector<ratio_one_unit> target(10000, 0);
+      for (size_t i = 0; i < source.size(); ++i) {
+        source[i] = i;
+      }
+
+      meter.measure([&] {
+        for (size_t i = 0; i < source.size(); ++i) {
+          target[i] -= source[i];
+        }
+        return target;
+      });
+    };
+    BENCHMARK_ADVANCED("Same unit different ratio subtract assignment")
+    (Catch::Benchmark::Chronometer meter) {
+      std::vector<ratio_one_unit> source(10000);
+      std::vector<ratio_milli_unit> target(10000, 0);
+      for (size_t i = 0; i < source.size(); ++i) {
+        source[i] = i;
+      }
+
+      meter.measure([&] {
+        for (size_t i = 0; i < source.size(); ++i) {
+          target[i] -= source[i];
+        }
+        return target;
+      });
+    };
+  }
+
+  // SECTION("multiply-Assignment") {
+  //   BENCHMARK_ADVANCED("Raw number multiply assignment (reference)")
+  //   (Catch::Benchmark::Chronometer meter) {
+  //     std::vector<int64_t> source(10000);
+  //     std::vector<int64_t> target(10000, 0);
+  //     for (size_t i = 0; i < source.size(); ++i) {
+  //       source[i] = i;
+  //     }
+
+  //     meter.measure([&] {
+  //       for (size_t i = 0; i < source.size(); ++i) {
+  //         target[i] *= source[i];
+  //       }
+  //       return target;
+  //     });
+  //   };
+  //   BENCHMARK_ADVANCED("Same unit same ratio multiply assignment")
+  //   (Catch::Benchmark::Chronometer meter) {
+  //     std::vector<ratio_one_unit> source(10000);
+  //     std::vector<ratio_one_unit> target(10000, 0);
+  //     for (size_t i = 0; i < source.size(); ++i) {
+  //       source[i] = i;
+  //     }
+
+  //     meter.measure([&] {
+  //       for (size_t i = 0; i < source.size(); ++i) {
+  //         target[i] *= source[i];
+  //       }
+  //       return target;
+  //     });
+  //   };
+  //   BENCHMARK_ADVANCED("Same unit different ratio multiply assignment")
+  //   (Catch::Benchmark::Chronometer meter) {
+  //     std::vector<ratio_one_unit> source(10000);
+  //     std::vector<ratio_milli_unit> target(10000, 0);
+  //     for (size_t i = 0; i < source.size(); ++i) {
+  //       source[i] = i;
+  //     }
+
+  //     meter.measure([&] {
+  //       for (size_t i = 0; i < source.size(); ++i) {
+  //         target[i] *= source[i];
+  //       }
+  //       return target;
+  //     });
+  //   };
+  // }
 }
