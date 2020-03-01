7afdfe3bba3782310ed5287f1d9ca2b1521f7080
Dominik Berner <dominik.berner@gmail.com>
2020-03-01 22:13:02 +0100

    Add test for assignment

--- test/src/detail_tests/unit_t_conversions_tests.cc
+++ test/src/detail_tests/unit_t_conversions_tests.cc
@@ -13,4 +13,17 @@ TEST_CASE("GIVEN a declared unit of type int64_t WHEN copy-constructed "
   constexpr unit_t<'X', 1, int64_t> v_int(v_int8);
 
   STATIC_REQUIRE(std::is_same<decltype(v_int)::internal_type, int64_t>::value);
+  STATIC_REQUIRE(v_int.raw_value() == 1);
 }
+
+TEST_CASE("GIVEN a declared unit of long double WHEN assigned a unit of type "
+          "int64_t THEN value is implicitely converted") {
+  constexpr unit_t<'X', 1, int64_t> v_int64{1};
+  constexpr unit_t<'X', 1, long double> v_double = v_int64;
+
+  STATIC_REQUIRE(
+      std::is_same<decltype(v_double)::internal_type, long double>::value);
+}
+
+  double dd = i;
+  REQUIRE(dd > 0);
