7e2e1cb69faa07a816dd2457ef9a5f3558eb9627
Dominik Berner <dominik.berner@gmail.com>
2020-08-12 22:29:28 +0200

    Remove conan from version consitency check
    Because conan pulls it from CMakeLists.txt

--- test/version-consistency.sh
+++ test/version-consistency.sh
@@ -13,7 +13,7 @@ tearDown() {
     if [ -d ${SI_BUILD_DIR} ]; then
         rm -rf ${SI_BUILD_DIR}
     fi
-
+    
     if [ -d ${BUILD_DIR} ]; then
         rm -rf ${BUILD_DIR}
     fi
@@ -30,26 +30,23 @@ testVersionNumberConsistency() {
     cmake ${ROOT_DIR} -B${SI_BUILD_DIR} -DBUILD_TESTING=off -DCMAKE_BUILD_TYPE=Debug >/dev/null
     cd ${SI_BUILD_DIR}
     CMAKE_VERSION=$(cmake --system-information | grep -E "VERSION:STATIC" | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+')
-    cd ${ROOT_DIR}
-    CONAN_VERSION=$(${PYTHON_CMD} -c 'from conanfile import SiConan; print(SiConan.version)')
     cd ${ORIG_DIR}
     GIT_VERSION_EXACT=$(git describe --tags | grep -E -o '^[0-9]+\.[0-9]+\.[0-9]+$')
-
+    
     assertEquals "version in changelog (${CHANGELOG_VERSION}) does not match cmake version (${CMAKE_VERSION})" $CHANGELOG_VERSION $CMAKE_VERSION
-    assertEquals "version in cmake (${CMAKE_VERSION}) does not match conan version (${CONAN_VERSION})" $CMAKE_VERSION $CONAN_VERSION
-
+    
     if [ "${GIT_VERSION_EXACT}" != "" ]; then
         assertEquals "version in files (${CMAKE_VERSION}) does not match git tag for release  (${GIT_VERSION_EXACT})" ${CMAKE_VERSION} ${GIT_VERSION_EXACT}
     else
         GIT_VERSION=$(git describe --tags | grep -E -o '^[0-9]+\.[0-9]+\.[0-9]+')
         assertNotEquals "version in files (${CMAKE_VERSION}) matches already existing release (${GIT_VERSION}):" ${CMAKE_VERSION} ${GIT_VERSION}
     fi
-
+    
     for F in $(find ${ROOT_DIR}/include/SI -name "*.h"); do
         FILE_VERSION=$(grep -E 'version [0-9]+\.[0-9]+\.[0-9]+' ${F} | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+')
         assertEquals "version in header file ${F} does not match cmake version" ${CMAKE_VERSION} ${FILE_VERSION}
     done
-
+    
 }
 
 # Load shUnit2.
