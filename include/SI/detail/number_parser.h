8a0ce617bab884345db84a1552c517271d54a8cd
Dominik Berner <dominik.berner@gmail.com>
2020-08-20 17:14:04 +0200

    Using namespace for intmax

--- include/SI/detail/number_parser.h
+++ include/SI/detail/number_parser.h
@@ -18,7 +18,7 @@ namespace SI::detail::parsing {
 
 /// struct converting a char digit into an int
 /// allowed digits are [0-9][a-fA-F][']
-template <intmax_t _base, char _str_digit> struct Digit_impl {
+template <std::intmax_t _base, char _str_digit> struct Digit_impl {
   static_assert((_str_digit >= '0' && _str_digit <= '9') ||
                 (_str_digit >= 'a' && _str_digit <= 'f') ||
                 (_str_digit >= 'A' && _str_digit <= 'F') || _str_digit == '\'');
@@ -27,69 +27,70 @@ template <intmax_t _base, char _str_digit> struct Digit_impl {
   static_assert(_base < 17, "maximum representation is hex (base == 16)");
 
   static constexpr bool is_valid_digit = _str_digit == '\'' ? false : true;
-  static constexpr intmax_t value =
+  static constexpr std::intmax_t value =
       (_str_digit >= '0' && _str_digit <= '9')
           ? _str_digit - '0'
           : (_str_digit >= 'a' && _str_digit <= 'f')
                 ? 10 + (_str_digit - 'a')
                 : (_str_digit >= 'A' && _str_digit <= 'F')
                       ? 10 + (_str_digit - 'A')
-                      : std::numeric_limits<intmax_t>::quiet_NaN();
+                      : std::numeric_limits<std::intmax_t>::quiet_NaN();
   static_assert(!is_valid_digit || value < _base, "Digit is valid for base");
 };
 
 // Interface class to access Digit
-template <intmax_t _base, char _str_digit>
+template <std::intmax_t _base, char _str_digit>
 struct Digit : public Digit_impl<_base, _str_digit> {};
 
 /// Struct containing a power to a base; used as multiplicator of each position
 /// in a number
-template <intmax_t _base, char _digit, char... _digits> struct Power_impl {
+template <std::intmax_t _base, char _digit, char... _digits> struct Power_impl {
 
   using digit = Digit<_base, _digit>;
   using recursive_power = Power_impl<_base, _digits...>;
-  static constexpr intmax_t power = digit::is_valid_digit
-                                        ? (recursive_power::power * _base)
-                                        : recursive_power::power;
+  static constexpr std::intmax_t power = digit::is_valid_digit
+                                             ? (recursive_power::power * _base)
+                                             : recursive_power::power;
 };
 
 /// terminating case for power variadic template
-template <intmax_t _base, char _digit> struct Power_impl<_base, _digit> {
-  static constexpr intmax_t power = 1;
+template <std::intmax_t _base, char _digit> struct Power_impl<_base, _digit> {
+  static constexpr std::intmax_t power = 1;
 };
 
 /// interface class for power calculation
-template <intmax_t _base, char... _digits>
+template <std::intmax_t _base, char... _digits>
 struct Power : Power_impl<_base, _digits...> {};
 
 /// Edge case for _base^0 == 1 s multiplier
-template <intmax_t _base> struct Power<_base> {
-  static constexpr intmax_t power = 1;
+template <std::intmax_t _base> struct Power<_base> {
+  static constexpr std::intmax_t power = 1;
 };
 
-template <intmax_t _base, char _digit, char... _digits> struct Magnitude {
+template <std::intmax_t _base, char _digit, char... _digits> struct Magnitude {
   using digit = Digit<_base, _digit>;
-  static constexpr intmax_t recursive_magnitude =
+  static constexpr std::intmax_t recursive_magnitude =
       Magnitude<_base, _digits...>::value;
-  static constexpr intmax_t value =
+  static constexpr std::intmax_t value =
       digit::is_valid_digit ? 1 + recursive_magnitude : recursive_magnitude;
 };
 
-template <intmax_t _base, char _digit> struct Magnitude<_base, _digit> {
-  static constexpr intmax_t value = 0;
+template <std::intmax_t _base, char _digit> struct Magnitude<_base, _digit> {
+  static constexpr std::intmax_t value = 0;
 };
 
 /// recursive struct that builds the number
-template <intmax_t _base, char _digit, char... _digits> struct Number_impl {
+template <std::intmax_t _base, char _digit, char... _digits>
+struct Number_impl {
 
-  static constexpr intmax_t base = _base;
+  static constexpr std::intmax_t base = _base;
   using digit = Digit<base, _digit>;
-  static constexpr intmax_t magnitude =
+  static constexpr std::intmax_t magnitude =
       Magnitude<_base, _digit, _digits...>::value;
-  static constexpr intmax_t power = Power<base, _digit, _digits...>::power;
+  static constexpr std::intmax_t power = Power<base, _digit, _digits...>::power;
 
   using recursive_number = Number_impl<_base, _digits...>;
-  static constexpr intmax_t value =
+  static constexpr std::intmax_t value =
       recursive_number::value +
       (digit::is_valid_digit ? (digit::value * power) : 0);
   static_assert(!digit::is_valid_digit || value / power == digit::value,
@@ -97,14 +98,14 @@ template <intmax_t _base, char _digit, char... _digits> struct Number_impl {
 };
 
 /// terminating case for variadic template
-template <intmax_t _base, char _digit> struct Number_impl<_base, _digit> {
+template <std::intmax_t _base, char _digit> struct Number_impl<_base, _digit> {
 
   using digit = Digit<_base, _digit>;
   static_assert(digit::is_valid_digit, "digit is valid");
-  static constexpr intmax_t value = digit::value;
-  static constexpr intmax_t magnitude = 0;
-  static constexpr intmax_t base = _base;
-  static constexpr intmax_t power = Power<base>::power;
+  static constexpr std::intmax_t value = digit::value;
+  static constexpr std::intmax_t magnitude = 0;
+  static constexpr std::intmax_t base = _base;
+  static constexpr std::intmax_t power = Power<base>::power;
   static_assert(power == 1, "power should be one");
 };
 
