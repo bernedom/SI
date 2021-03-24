33d122e556e04ab305e7a3ba0c9576f48816cb8c
Dominik Berner <dominik.berner@gmail.com>
2021-03-24 08:53:39 +0100

    Change version to 2.1.0

--- include/SI/force.h
+++ include/SI/force.h
@@ -1,6 +1,6 @@
 
 /**
- * This file is part of "SI" version 2.0.5
+ * This file is part of "SI" version 2.1.0
  * A header only c++ library that provides type safety and user defined literals
  * for handling pyhsical values defined in the International System of
  * Units
