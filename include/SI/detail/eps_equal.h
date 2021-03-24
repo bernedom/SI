2a52d29f03d09bc864d063acfb974a2a4e7140a4
Dominik Berner <dominik.berner@gmail.com>
2021-03-24 09:17:27 +0100

    Add license information to header

--- include/SI/detail/eps_equal.h
+++ include/SI/detail/eps_equal.h
@@ -1,3 +1,14 @@
+/**
+ * This file is part of "SI" version 2.1.0
+ * A header only c++ library that provides type safety and user defined literals
+ * for handling pyhsical values defined in the International System of
+ * Units
+ *
+ * https://github.com/bernedom/SI
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ **/
 #pragma once
 
 #include <limits>
