c442f8cec43eb6e1083f8549bf67b6de1ae61e91
Dominik Berner <dominik.berner@gmail.com>
2021-11-27 23:16:59 +0100

    Add update script

---
+++ .github/actions/conancenterindex/updater.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+import yaml
+import io
+import requests
+import hashlib
+
+
+with open("config.yml", 'r') as stream:
+    data_loaded = yaml.safe_load(stream)
+
+print(data_loaded["versions"])
+new_version = dict()
+data_loaded["versions"]["2.1.4"] = {'folder': 'all'}
+
+with io.open('config_data.yaml', 'w', encoding='utf8') as outfile:
+    yaml.dump(data_loaded, outfile,
+              default_flow_style=False, allow_unicode=True)
+
+r = requests.get(
+    "https://github.com/bernedom/SI/archive/2.1.3.tar.gz", allow_redirects=True)
+hash = hashlib.sha256(r.content).hexdigest()
+print(hash)
+
+with open("all/conandata.yml", 'r') as stream:
+    conandata = yaml.safe_load(stream)
+
+print(conandata)
+conandata["sources"]["2.1.4"] = {
+    'url': 'https://github.com/bernedom/SI/archive/2.1.3.tar.gz', 'sha256': hash}
+
+with io.open('conandata_data.yaml', 'w', encoding='utf8') as outfile:
+    yaml.dump(conandata, outfile,
+              default_flow_style=False, allow_unicode=True)
