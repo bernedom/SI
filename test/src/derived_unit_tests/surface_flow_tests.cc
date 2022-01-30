7aa1444ec2428250d0d17cfce414ed44ee9c1b9f
Dominik Berner <dominik.berner@gmail.com>
2022-01-30 11:15:52 +0100

    Add volumetric flow

--- test/src/derived_unit_tests/surface_flow_tests.cc
+++ test/src/derived_unit_tests/surface_flow_tests.cc
@@ -36,3 +36,33 @@ TEMPLATE_TEST_CASE(
   STATIC_REQUIRE(
       std::is_same<decltype(result), decltype(result_commutative)>::value);
 }
+
+TEST_CASE("GIVEN an volume value WHEN deivided by a time value THEN the result "
+          "is a surface_flow") {
+  constexpr auto volume = 1_m3;
+  constexpr auto time = 1_s;
+  constexpr auto result = volume / time;
+
+  STATIC_REQUIRE(
+      std::is_same<decltype(result),
+                   const SI::volumetric_flow_t<int64_t, std::ratio<1>>>::value);
+}
+
+TEMPLATE_TEST_CASE(
+    "GIVEN surface_flow value WHEN multiplied with time THEN result is a "
+    "volume value",
+    "[length][operator_tests]", int64_t, long double) {
+
+  constexpr SI::volumetric_flow_t<TestType, std::ratio<1>> v{1};
+  constexpr SI::time_t<TestType, std::ratio<1>> T{1};
+
+  constexpr auto result = v * T;
+  constexpr auto result_commutative = T * v;
+
+  STATIC_REQUIRE(
+      std::is_same<decltype(result),
+                   const SI::volume_t<TestType, std::ratio<1>>>::value);
+
+  STATIC_REQUIRE(
+      std::is_same<decltype(result), decltype(result_commutative)>::value);
+}
