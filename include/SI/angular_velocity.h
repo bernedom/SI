aa53695f275f5b5093c71892e57b6cd9a446063a
Dominik Berner <dominik.berner@wingtra.com>
2022-12-23 16:46:22 +0100

    Add angular velocity

---
+++ include/SI/angular_velocity.h
@@ -0,0 +1,30 @@
+/**
+ * This file is part of "SI" version 2.5.2
+ * A header only c++ library that provides type safety and user defined literals
+ * for handling physical values defined in the International System of
+ * Units
+ *
+ * https://github.com/bernedom/SI
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ **/
+#pragma once
+
+#include "angle.h"
+#include "detail/operator_helpers.h"
+#include "detail/unit.h"
+#include "frequency.h"
+#include "time.h"
+
+namespace SI {
+
+/// Type for velocity where w =  r / T
+template <typename _type, typename _ratio>
+using angular_velocity_t = detail::unit_t<'w', std::ratio<1>, _type, _ratio>;
+
+namespace detail {
+BUILD_UNIT_FROM_MULTIPLICATION(angular_velocity_t, angle_t, frequency_t)
+BUILD_UNIT_FROM_DIVISION(angular_velocity_t, angle_t, time_t)
+} // namespace detail
+} // namespace SI
---
+++ test/src/derived_unit_tests/angular_velocity_tests.cc
@@ -0,0 +1,33 @@
+#include <catch2/catch.hpp>
+
+#include <SI/angle.h>
+#include <SI/angular_velocity.h>
+#include <SI/frequency.h>
+#include <type_traits>
+
+using namespace SI;
+
+TEST_CASE("GIVEN an angle type WHEN divided by a time type THEN result is "
+          "angular velocity type") {
+  constexpr auto value = 1_rad / 1_s;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(value),
+                 const SI::angular_velocity_t<int64_t, std::ratio<1>>>::value);
+}
+
+TEST_CASE("GIVEN a frequency type WHEN multiplied by an angle type THEN result "
+          "is angle/per time") {
+  constexpr auto value = 1_Hz * 1_rad;
+  STATIC_REQUIRE(std::is_same<
+                 decltype(value),
+                 const SI::angular_velocity_t<int64_t, std::ratio<1>>>::value);
+}
+
+// TEST_CASE("GIVEN a frequency type WHEN multiplied by an angle type THEN
+// result "
+//           "is angle/per time") {
+//   constexpr auto value = 1_Hz * 1_rad;
+//   STATIC_REQUIRE(std::is_same<decltype(value),
+//                               const SI::angle_t<int64_t, std::ratio<1, 1>,
+//                                                 std::ratio<1, 1>>>::value);
+// }
