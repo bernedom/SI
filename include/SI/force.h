ceeb82e77674d057fde09693599b7992e1e49d50
Dominik Berner <dominik.berner@gmail.com>
2022-12-27 23:27:07 +0100

    Version bump to 2.5.2

--- include/SI/force.h
+++ include/SI/force.h
@@ -1,6 +1,6 @@
 
 /**
- * This file is part of "SI" version 2.5.1
+ * This file is part of "SI" version 2.5.2
  * A header only c++ library that provides type safety and user defined literals
  * for handling physical values defined in the International System of
  * Units
