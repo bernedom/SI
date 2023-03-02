b57f5cf85ced312190531ea61de35461d0a0c9a9
Dominik Berner <dominik.berner@gmail.com>
2023-03-02 22:16:55 +0100

    Rename angular velocity to angular frequency

--- include/SI/angular_frequency.h
+++ include/SI/angular_frequency.h
@@ -21,10 +21,10 @@ namespace SI {
 
 /// Type for velocity where w =  r / T
 template <typename _type, typename _ratio>
-using angular_velocity_t = detail::unit_t<'w', std::ratio<1>, _type, _ratio>;
+using angular_frequency_t = detail::unit_t<'w', std::ratio<1>, _type, _ratio>;
 
 namespace detail {
-BUILD_UNIT_FROM_MULTIPLICATION(angular_velocity_t, angle_t, frequency_t)
-BUILD_UNIT_FROM_DIVISION(angular_velocity_t, angle_t, time_t)
+BUILD_UNIT_FROM_MULTIPLICATION(angular_frequency_t, angle_t, frequency_t)
+BUILD_UNIT_FROM_DIVISION(angular_frequency_t, angle_t, time_t)
 } // namespace detail
 } // namespace SI
--- test/src/derived_unit_tests/angular_frequency_tests.cc
+++ test/src/derived_unit_tests/angular_frequency_tests.cc
@@ -1,18 +1,18 @@
 #include <catch2/catch.hpp>
 
 #include <SI/angle.h>
-#include <SI/angular_velocity.h>
+#include <SI/angular_frequency.h>
 #include <SI/frequency.h>
 #include <type_traits>
 
 using namespace SI;
 
 TEST_CASE("GIVEN an angle type WHEN divided by a time type THEN result is "
-          "angular velocity type") {
+          "angular frequency type") {
   constexpr auto value = 1_rad / 1_s;
   STATIC_REQUIRE(std::is_same<
                  decltype(value),
-                 const SI::angular_velocity_t<int64_t, std::ratio<1>>>::value);
+                 const SI::angular_frequency_t<int64_t, std::ratio<1>>>::value);
 }
 
 TEST_CASE("GIVEN a frequency type WHEN multiplied by an angle type THEN result "
@@ -20,7 +20,7 @@ TEST_CASE("GIVEN a frequency type WHEN multiplied by an angle type THEN result "
   constexpr auto value = 1_Hz * 1_rad;
   STATIC_REQUIRE(std::is_same<
                  decltype(value),
-                 const SI::angular_velocity_t<int64_t, std::ratio<1>>>::value);
+                 const SI::angular_frequency_t<int64_t, std::ratio<1>>>::value);
 }
 
 // TEST_CASE("GIVEN a frequency type WHEN multiplied by an angle type THEN
