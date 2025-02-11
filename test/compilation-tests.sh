ab4e93fc51d08deeceaf92b79b7fb8ff849e9635
Dominik Berner <dominik.berner@gmail.com>
2025-02-11 21:59:25 +0100

    Pass test name to buildsingletarget

--- test/compilation-tests.sh
+++ test/compilation-tests.sh
@@ -10,11 +10,11 @@ buildSingleTarget()
         set -x
     if [ "${2}" == "DEFAULTBUILD" ]; then
         cmake ${ROOT_DIR}/test/src/compilation_tests/ -B${BUILD_DIR} -DCMAKE_PREFIX_PATH=${BUILD_DIR} -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PATH} -DCMAKE_BUILD_TYPE=Release -G Ninja > /dev/null
-        assertEquals "Configuration for defaultbuild successful" 0 $?
+        assertEquals "Configuration for defaultbuild successful" 0 $? ${3}
 
     else
         cmake ${ROOT_DIR}/test/src/compilation_tests/ -B${BUILD_DIR} -DCMAKE_PREFIX_PATH=${BUILD_DIR} -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PATH} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-DSI_DISABLE_IMPLICIT_RATIO_CONVERSION" -G Ninja > /dev/null
-        assertEquals "Configuration for build with disabled ration cinversion successful" 0 $?
+        assertEquals "Configuration for build with disabled ration cinversion successful" 0 $? ${3}
     fi
 
     cmake --build ${BUILD_DIR} --config Release --target $1 > /dev/null
@@ -72,53 +72,53 @@ testSISelfFailedCompilationWhenImplicitConversionDisabled() {
 testOperatorCopyCtorWithSameRatioCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/copy_ctor_with_same_ratio_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorCopyCtorWithSameRatioCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorCopyCtorWithSameRatioCompilesWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/copy_ctor_with_same_ratio_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS "testOperatorCopyCtorWithSameRatioCompilesWhenImplicitConversionDisabled"
 }
 
 testOperatorCopyCtorCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/copy_ctor_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorCopyCtorCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorCopyCtorFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/copy_ctor_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorCopyCtorFailsWhenImplicitConversionDisabled"
 }
 
 testOperatorMoveCtorWithSameRatioCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/move_ctor_with_same_ratio_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorMoveCtorWithSameRatioCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorMoveCtorWithSameRatioCompilesWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/move_ctor_with_same_ratio_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS "testOperatorMoveCtorWithSameRatioCompilesWhenImplicitConversionDisabled"
 }
 
 testOperatorMoveCtorCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/move_ctor_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorMoveCtorCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorMoveCtorFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/move_ctor_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorMoveCtorFailsWhenImplicitConversionDisabled"
 }
 
 
@@ -126,171 +126,171 @@ testOperatorMoveCtorFailsWhenImplicitConversionDisabled()
 testOperatorCopyAssignmentCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_copy_assignment_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorCopyAssignmentCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorCopyAssignmentFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_copy_assignment_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorCopyAssignmentFailsWhenImplicitConversionDisabled"
 }
 
 testOperatorCopyAssignmentWithSameRatioCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_copy_assignment_same_ratio_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorCopyAssignmentWithSameRatioCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorCopyAssignmentWithSameRatioCompilesWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_copy_assignment_same_ratio_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS "testOperatorCopyAssignmentWithSameRatioCompilesWhenImplicitConversionDisabled"
 }
 
 testOperatorMoveAssignmentCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_move_assignment_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorMoveAssignmentCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorMoveAssignmentFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_move_assignment_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorMoveAssignmentFailsWhenImplicitConversionDisabled"
 }
 
 testOperatorMoveAssignmentWithSameRatioCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_move_assignment_same_ratio_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorMoveAssignmentWithSameRatioCompilesWhenDefaultInvocation"
 }
 
 testOperatorMoveAssignmentWithSameRatioCompilesWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_move_assignment_same_ratio_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS "testOperatorMoveAssignmentWithSameRatioCompilesWhenImplicitConversionDisabled"
 }
 
 testOperatorEqualsCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_equals_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorEqualsCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorEqualsFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_equals_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorEqualsFailsWhenImplicitConversionDisabled"
 }
 
 
 testOperatorLessThanCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_less_than_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorLessThanCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorLessThanFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_less_than_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorLessThanFailsWhenImplicitConversionDisabled"
 }
 
 testOperatorGreaterThanCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_greater_than_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorGreaterThanCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorGreaterThanFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_greater_than_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorGreaterThanFailsWhenImplicitConversionDisabled"
 }
 
 testOperatorMultiplyCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_multiply_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorMultiplyCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorMultiplyFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_multiply_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorMultiplyFailsWhenImplicitConversionDisabled"
 }
 
 testOperatorAddCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_add_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorAddCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorAddFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_add_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorAddFailsWhenImplicitConversionDisabled"
 }
 
 
 testOperatorAddAssignmentCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_add_assignment_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorAddAssignmentCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorAddAssignmentFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_add_assignment_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorAddAssignmentFailsWhenImplicitConversionDisabled"
 }
 
 testOperatorAddAssignmentSameRatioCompilesWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_add_assignment_same_ratio_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS "testOperatorAddAssignmentSameRatioCompilesWhenImplicitConversionDisabled"
 }
 
 
 testOperatorSubtractCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_subtract_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorSubtractCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorSubtractFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_subtract_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorSubtractFailsWhenImplicitConversionDisabled"
 }
 
 testOperatorSubtractAssignmentCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_subtract_assignment_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorSubtractAssignmentCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorSubtractAssignmentFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_subtract_assignment_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorSubtractAssignmentFailsWhenImplicitConversionDisabled"
 }
 
 testOperatorSubtractAssignmentWithSameRatioCompilesWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_subtract_assignment_same_ratio_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD PASS "testOperatorSubtractAssignmentWithSameRatioCompilesWhenImplicitConversionDisabled"
 }
 
 
@@ -298,53 +298,53 @@ testOperatorSubtractAssignmentWithSameRatioCompilesWhenImplicitConversionDisable
 testOperatorDivideCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_divide_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorDivideCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorDivideFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_divide_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorDivideFailsWhenImplicitConversionDisabled"
 }
 
 testOperatorDivideFromFloatingPointCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_divide_from_floating_point_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorDivideFromFloatingPointCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorDivideFromFloatingPointFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_divide_from_floating_point_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorDivideFromFloatingPointFailsWhenImplicitConversionDisabled"
 }
 
 
 testOperatorDivideFromIntegralCompilesWhenDefaultInvocation()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_divide_from_integral_test.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testOperatorDivideFromIntegralCompilesWhenDefaultInvocation"
 }
 
 
 testOperatorDivideFromIntegralFailsWhenImplicitConversionDisabled()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/operator_divide_from_integral_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testOperatorDivideFromIntegralFailsWhenImplicitConversionDisabled"
 }
 
 testCompilationFailsWhenNumberParserOverflows()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/parsing_overflow_test.cc.o
-    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL
+    buildSingleTarget ${TARGET} RESTRICTEDBUILD FAIL "testCompilationFailsWhenNumberParserOverflows"
 }
 
 testCompilationSucceedsWhenNumberParserDoesNotOverflow()
 {
     TARGET=CMakeFiles/SI-Compilation-Tests.dir/parsing_overflow_test_pass_compilation.cc.o
-    buildSingleTarget ${TARGET} DEFAULTBUILD PASS
+    buildSingleTarget ${TARGET} DEFAULTBUILD PASS "testCompilationSucceedsWhenNumberParserDoesNotOverflow"
 }
 
 
